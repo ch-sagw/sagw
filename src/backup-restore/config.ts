d81366ba4734115b23c3330532f1355ec4f8bf86
Yves Tscherry <info@feerglas.com>
2025-04-05 17:10:50 +0200

    chore: keep backups for 1 month

--- src/backup-restore/config.ts
+++ src/backup-restore/config.ts
@@ -1,7 +1,7 @@
 export default {
   blobBackupBucketPrefix: 'blob-backup',
   dbBackupBucketPrefix: 'db-backup',
-  keepAmountOfBackups: 4,
+  keepAmountOfBackups: 30,
   sendMailOnFailure: false,
   sendMailOnSuccess: false,
   sendSlackOnFailure: true,
