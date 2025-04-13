8038e4d7f4b44d1ca1efd4dd137581903b162a5b
Yves Tscherry <info@feerglas.com>
2025-04-13 12:22:18 +0200

    chore: remove comments

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,18 +1,7 @@
-// This file configures the initialization of Sentry on the server.
-// The config you add here will be used whenever the server handles a request.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
 import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  // Setting this option to true will print useful information to the console
-  // while you're setting up Sentry.
   debug: false,
-
   dsn: 'https://ff825b055c033f18e44cc08059197dc5@o4509054769299456.ingest.de.sentry.io/4509085121773648',
-
-  // Define how likely traces are sampled. Adjust this value in production,
-  // or use tracesSampler for greater control.
   tracesSampleRate: 1,
-
 });
