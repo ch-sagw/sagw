f970c23d2a0fed94d62657588c78f05e0c685bcb
Yves Tscherry <info@feerglas.com>
2025-04-13 12:21:41 +0200

    fix: set correct public server url for vercel env

--- next.config.mjs
+++ next.config.mjs
@@ -7,10 +7,9 @@ import { withPayload } from '@payloadcms/next/withPayload';
 const rootFileName = fileURLToPath(import.meta.url);
 const rootDirName = path.dirname(rootFileName);
 
-// const NEXT_PUBLIC_SERVER_URL = process.env.URL
-// || process.env.DEPLOY_URL || 'http://localhost:3000';
-
-const NEXT_PUBLIC_SERVER_URL = 'http://localhost:3000';
+const NEXT_PUBLIC_SERVER_URL = process.env.URL ||
+  process.env.DEPLOY_URL ||
+  'http://localhost:3000';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -42,36 +41,11 @@ export default withSentryConfig(configWithPayload, {
   devBundleServerPackages: false,
 }, {
   authToken: process.env.SENTRY_AUTH_TOKEN,
-
-  // For all available options, see:
-  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
-  // Enables automatic instrumentation of Vercel Cron Monitors.
-  // (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
   automaticVercelMonitors: true,
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
   disableLogger: true,
   org: process.env.SENTRY_PROJECT,
   project: process.env.SENTRY_ORG,
-
-  // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
-
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  // Route browser requests to Sentry through a Next.js rewrite to
-  // circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your
-  // Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
   tunnelRoute: '/monitoring',
-
-  // Upload a larger set of source maps for prettier stack traces
-  // (increases build time)
   widenClientFileUpload: true,
 });
