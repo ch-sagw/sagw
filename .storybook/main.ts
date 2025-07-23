b39e132bc2da89559bf1a5c655c704af5e05eeff
Marius Bleuer <m.bleuer@lycos.com>
2025-07-23 17:50:40 +0200

    chore: resolving difference with remote branch

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,8 @@
 import type { StorybookConfig } from '@storybook/nextjs';
+import path from 'path';
 
 const config: StorybookConfig = {
-  addons: ['@storybook/addon-essentials'],
+  addons: ['@storybook/addon-docs'],
   features: {
     experimentalRSC: true,
   },
@@ -13,6 +14,16 @@ const config: StorybookConfig = {
     '../src/**/*.mdx',
     '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
+  // eslint-disable-next-line require-await
+  webpackFinal: async (webpackConfig) => {
+    webpackConfig.resolve = webpackConfig.resolve || {};
+    webpackConfig.resolve.alias = {
+      ...(webpackConfig.resolve.alias || {}),
+      '@styles': path.resolve(__dirname, '../src/styles'),
+    };
+
+    return webpackConfig;
+  },
 };
 
 export default config;
