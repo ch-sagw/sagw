725acb72e32adf7301a18a6fb7e750336b9e8e1f
Yves Tscherry <info@feerglas.com>
2025-08-08 17:11:10 +0200

    Revert "feat: add helper to generate url"
    This reverts commit bd0d64c4da2f9c6372aa26681a836a0448ec0064.

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,28 +1,6 @@
 import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob';
-import { seoPlugin } from '@payloadcms/plugin-seo';
-import {
-  GenerateTitle, GenerateURL,
-} from '@payloadcms/plugin-seo/types';
-
 import { Plugin } from 'payload';
 import { Media } from '@/collections/Media';
-import { getServerSideURL } from '@/utilities/getUrl';
-
-const generateTitle: GenerateTitle = ({
-  doc,
-}) => (doc?.title
-  ? `${doc.title} | SAGW`
-  : 'SAGW');
-
-const generateURL: GenerateURL = ({
-  doc,
-}) => {
-  const url = getServerSideURL();
-
-  return doc?.slug
-    ? `${url}/${doc.slug}`
-    : url;
-};
 
 const plugins: Plugin[] = [
   vercelBlobStorage({
@@ -32,10 +10,6 @@ const plugins: Plugin[] = [
     },
     enabled: true,
     token: process.env.BLOB_READ_WRITE_TOKEN,
-  }),
-  seoPlugin({
-    generateTitle,
-    generateURL,
   }),
 ];
 
