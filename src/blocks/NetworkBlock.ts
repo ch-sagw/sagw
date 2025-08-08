f5a6a4a47bc432b509c826969a073164aa11f439
Yves Tscherry <info@feerglas.com>
2025-08-08 17:06:40 +0200

    feat: add various blocks

---
+++ src/blocks/NetworkBlock.ts
@@ -0,0 +1,39 @@
+import { Block } from 'payload';
+
+export const NetworkBlock: Block = {
+  fields: [
+    {
+      localized: true,
+      name: 'title',
+      required: true,
+      type: 'text',
+    },
+    {
+      name: 'category',
+      relationTo: 'networkCategories',
+      required: true,
+      type: 'relationship',
+    },
+    {
+      name: 'foundingYear',
+      required: true,
+      type: 'number',
+    },
+    {
+      name: 'image',
+      relationTo: 'media',
+      required: true,
+      type: 'upload',
+    },
+    {
+      name: 'link',
+      required: true,
+      type: 'text',
+    },
+  ],
+  labels: {
+    plural: 'Network Blocks',
+    singular: 'Network Block',
+  },
+  slug: 'networkBlock',
+};
