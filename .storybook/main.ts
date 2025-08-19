c0b6c0dcca29ed95f108a7fe555cf1590094e1eb
Yves Tscherry <info@feerglas.com>
2025-08-19 23:54:59 +0200

    chore: manually update and migrate storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,18 +1,15 @@
-import type { StorybookConfig } from '@storybook/nextjs';
+import type { StorybookConfig } from '@storybook/nextjs-vite';
 
 const config: StorybookConfig = {
-  addons: ['@storybook/addon-essentials'],
+  addons: ['@storybook/addon-docs'],
   features: {
     experimentalRSC: true,
   },
   framework: {
-    name: '@storybook/nextjs',
+    name: '@storybook/nextjs-vite',
     options: {},
   },
-  stories: [
-    '../src/**/*.mdx',
-    '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
-  ],
+  stories: ['../src/**/*.stories.@(ts|tsx)'],
 };
 
 export default config;
