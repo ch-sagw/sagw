cc62342208e33a96815b6669f5f956a62135dc95
Yves Tscherry <info@feerglas.com>
2025-08-20 12:00:10 +0200

    fix: set keep amount of backups to 3 weeks

--- src/backup-restore/config.ts
+++ src/backup-restore/config.ts
@@ -3,7 +3,7 @@ import '../../.env';
 export default {
   blobBackupBucketPrefix: `${process.env.BACKUP_RESTORE_PREFIX}-blob-backup`,
   dbBackupBucketPrefix: `${process.env.BACKUP_RESTORE_PREFIX}-db-backup`,
-  keepAmountOfBackups: 3,
+  keepAmountOfBackups: 21,
   maintenanceDownloadFolder: `${process.env.BACKUP_RESTORE_PREFIX}-download`,
   sendMailOnFailure: false,
   sendMailOnSuccess: false,
