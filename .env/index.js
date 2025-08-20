076bd271766db47eef14bb71386ec504f6345fa4
Yves Tscherry <info@feerglas.com>
2025-08-20 10:26:47 +0200

    chore: make dotenv quite

--- .env/index.js
+++ .env/index.js
@@ -11,12 +11,14 @@ if (process.env.CI !== 'true') {
   dotenv.config({
     override: true,
     path: '.env/.env.base',
+    quiet: true,
   });
 
   if (envs.includes(process.env.ENV)) {
     dotenv.config({
       override: true,
       path: `.env/.env.${process.env.ENV}`,
+      quiet: true,
     });
   }
 }
