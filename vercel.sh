c1f2fe9063da19241b0824fa28083bd5474047c3
Yves Tscherry <info@feerglas.com>
2025-08-21 20:28:55 +0200

    ci: try to fix build

--- vercel.sh
+++ vercel.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-if [[ $VERCEL_PROJECT_ID == "prj_kpN8C9D0xuT3v4qY561mmroJ6MSG"  ]] ; then
-  if [[ $VERCEL_TARGET_ENV == "production"  ]] ; then 
-    npm run build:prod
-  else 
-    npm run build
-  fi
+if [[ $VERCEL_TARGET_ENV == "production"  ]] ; then 
+  npm run build:prod
+else 
+  npm run build
 fi
