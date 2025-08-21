a457b0fef218695ed04b5be32a3ae398a69cadb3
Yves Tscherry <info@feerglas.com>
2025-08-21 09:03:41 +0200

    fix: reset maxduration to 300

--- src/app/api/cron-cleanup-backups/route.ts
+++ src/app/api/cron-cleanup-backups/route.ts
@@ -2,9 +2,10 @@ import type { NextRequest } from 'next/server';
 import cleanupBackups from '@/backup-restore/cron-jobs/backups-cleanup';
 import { getErrorMessage } from '@/backup-restore/helpers/try-catch-error';
 
-// defaults to 300. set it to max
+// defaults to 300. per docs, max should be 800. but vercel
+// returns an error if set to 800.
 // https://vercel.com/docs/functions/limitations#max-duration
-export const maxDuration = 800;
+export const maxDuration = 300;
 
 export const GET = async (request: NextRequest): Promise<Response> => {
   const authHeader = request.headers.get('authorization');
