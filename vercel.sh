7f921039e0c29dd5559aca4d90052d232711bae7
Yves Tscherry <info@feerglas.com>
2025-08-21 20:52:00 +0200

    ci: try to fix build

--- vercel.sh
+++ vercel.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # since we run 2 vercel projects (1 for payload / nextjs and 
-# one for storybook), we want `npm run build` only for payload.
+# one for storybook), we want `npm run build` only for payload,
+# and `npm run build-storybook` for storybook.
 
 if [[ $VERCEL_PROJECT_ID == "prj_kpN8C9D0xuT3v4qY561mmroJ6MSG"  ]] ; then
-  npm run build
+  npm run build:prod
+else 
+  npm run build-storybook
 fi
