2865aa682657cc376f36a763eae35a1aee87c225
Marius Bleuer <m.bleuer@lycos.com>
2025-08-25 16:57:22 +0200

    feat: allow and init sentry only on production

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,7 +1,9 @@
 import * as Sentry from '@sentry/nextjs';
 
-Sentry.init({
-  debug: false,
-  dsn: 'https://ff825b055c033f18e44cc08059197dc5@o4509054769299456.ingest.de.sentry.io/4509085121773648',
-  tracesSampleRate: 1,
-});
+if (process.env.SENTRY_DSN && process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    debug: false,
+    dsn: 'https://ff825b055c033f18e44cc08059197dc5@o4509054769299456.ingest.de.sentry.io/4509085121773648',
+    tracesSampleRate: 1,
+  });
+}
