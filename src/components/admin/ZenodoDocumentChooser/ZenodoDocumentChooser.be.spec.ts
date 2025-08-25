3ea62a7f8c0a737b8c687387d896cc9d810e076b
Yves Tscherry <info@feerglas.com>
2025-08-25 18:26:07 +0200

    test: add api test, better structure tests

--- src/components/admin/ZenodoDocumentChooser/ZenodoDocumentChooser.be.spec.ts
+++ src/components/admin/ZenodoDocumentChooser/ZenodoDocumentChooser.be.spec.ts
@@ -1,29 +1,18 @@
 import {
   expect,
-  test,
+  Locator, test,
 } from '@playwright/test';
 
-test.describe('Add Zenodo Document', () => {
-  test('step1', async ({
+test.describe('Add Zenodo document', () => {
+  let zenodoInput: Locator;
+  let zenodoButton: Locator;
+  let saveButton: Locator;
+  let zenodoList: Locator;
+
+  test.beforeEach(async ({
     page,
   }) => {
-
-    /*
-    await page.goto('http://localhost:3000/admin');
-    await page.waitForLoadState('load');
-
-    const zenodoDocuments = await page.getByText('Zenodo Documents')
-      .nth(1);
-
-    await zenodoDocuments.click({
-      force: true,
-    });
-
-    */
-    // await page.waitForURL('**/collections/zenodoDocuments');
-    // await page.waitForLoadState('domcontentloaded');
-
-    await page.goto('http://localhost:3000/admin/collections/zenodoDocuments');
+    await page.goto('http://localhost:3000/admin/collections/zenodoDocuments/create');
     await page.waitForLoadState('load');
 
     const createNewButton = await page.getByText('Neu erstellen', {
@@ -32,49 +21,63 @@ test.describe('Add Zenodo Document', () => {
 
     await createNewButton.click();
 
-    // 1. issue search and make sure that:
-    // - list is visible
-    // - list has 6 items
-    // - last item contains correct text
-
-    const zenodoInput = await page.getByTestId('zenodo-input');
-    const zenodoButton = await page.getByTestId('zenodo-button');
-    const saveButton = await page.getByRole('button', {
+    zenodoInput = page.getByTestId('zenodo-input');
+    zenodoButton = page.getByTestId('zenodo-button');
+    saveButton = page.getByRole('button', {
       name: 'Speichern',
     });
+    zenodoList = page.getByTestId('zenodo-list');
+  });
 
+  test('api returns proper data', async () => {
+    const foo = 'bar';
+
+    await expect(foo)
+      .toEqual('bar');
+  });
+
+  test('search and validate list items', async () => {
     await zenodoInput.fill('15126918');
     await zenodoButton.click();
 
-    const zenodoList = await page.getByTestId('zenodo-list');
-    const lastItem = await zenodoList.getByRole('listitem')
+    const lastItem = zenodoList.getByRole('listitem')
       .nth(5);
 
     await expect(zenodoList)
       .toBeVisible();
-
     await expect(lastItem)
       .toBeVisible();
-
     await expect(lastItem)
       .toHaveText('https://zenodo.org/api/records/15126918/files/source_data.xlsx/content (xlsx, 0.04 MB)');
+  });
 
-    // 2. change input and make sure that list disappears
+  test('results disappear after input change', async () => {
+    await zenodoInput.fill('15126918');
+    await zenodoButton.click();
+
+    await expect(zenodoList)
+      .toBeVisible();
 
     await zenodoInput.fill('1');
 
     await expect(zenodoList)
       .not.toBeVisible();
+  });
 
-    // 3. click save and expect validation error
+  test('validation error on invalid input', async ({
+    page,
+  }) => {
     await saveButton.click();
 
     const errorToast = await page.getByText('Die folgenden Felder sind nicht korrekt (4):');
 
     await expect(errorToast)
       .toBeVisible();
+  });
 
-    // 4. add zenodo id which is already existend in collection and expect error
+  test('validation error on existing zenodo id', async ({
+    page,
+  }) => {
     await zenodoInput.fill('1512691');
     await zenodoButton.click();
     await expect(zenodoList)
@@ -85,8 +88,11 @@ test.describe('Add Zenodo Document', () => {
 
     await expect(errorToast2)
       .toBeVisible();
+  });
 
-    // fill correct value, validate and save, expect success toast
+  test('sucessfull save on valid input', async ({
+    page,
+  }) => {
     await zenodoInput.fill('15126918');
     await zenodoButton.click();
     await expect(zenodoList)
@@ -97,9 +103,52 @@ test.describe('Add Zenodo Document', () => {
 
     await expect(successToast)
       .toBeVisible();
-
-    // make api call to check correct data
-
   });
 
+  test('returns proper api structure', async ({
+    page,
+  }) => {
+
+    // add a new document
+    await zenodoInput.fill('15126911');
+    await zenodoButton.click();
+    await expect(zenodoList)
+      .toBeVisible();
+    await saveButton.click();
+
+    const successToast = await page.getByText('Zenodo Document erfolgreich erstellt.');
+
+    await expect(successToast)
+      .toBeVisible();
+
+    // test the return value of the payload api
+    const res = await fetch('http://localhost:3000/api/zenodoDocuments?where[zenodoId][equals]=15126911');
+    const json = await res.json();
+
+    await expect(json.docs.length)
+      .toEqual(1);
+
+    const [doc] = json.docs;
+
+    await expect(doc.zenodoId)
+      .toEqual('15126911');
+
+    await expect(doc.title)
+      .toEqual('Initial Reports of the Deep Sea Drilling Project - Volume 6');
+
+    await expect(doc.publicationDate)
+      .toEqual('1971');
+
+    await expect(doc.files.length)
+      .toEqual(2);
+
+    await expect(doc.files[0].link)
+      .toEqual('https://zenodo.org/api/records/15126912/files/Leg 6 Volume.zip/content');
+
+    await expect(doc.files[0].format)
+      .toEqual('zip');
+
+    await expect(doc.files[0].size)
+      .toEqual(205.03);
+  });
 });
