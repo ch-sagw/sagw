4279dc0c2e2edf12e05e8f41f9ad3f02feeca77b
Yves Tscherry <info@feerglas.com>
2025-08-27 07:23:58 +0200

    test: add tests for internal link chooser

--- src/components/admin/InternalLinkChooser/InternalLink.be.spec.ts
+++ src/components/admin/InternalLinkChooser/InternalLink.be.spec.ts
@@ -1,26 +1,54 @@
-// http://localhost:3000/admin/collections/instituteDetail/create
-
 import {
   expect,
-  Locator,
   test,
 } from '@playwright/test';
 
 test.describe.serial('Internal Link Choosers', () => {
-  let linkTargetInput: Locator;
-
-  test.beforeEach(async ({
+  test('shows available links', async ({
     page,
   }) => {
     await page.goto('http://localhost:3000/admin/collections/instituteDetail/create');
     await page.waitForLoadState('load');
 
-    linkTargetInput = await page.getByLabel('Link Target');
+    const linkTargetInput = await page.getByLabel('Link Target');
+
+    await linkTargetInput.click();
+
+    const homePageLink = await page.getByText('Home Page');
+    const publicationLink = await page.getByText('Publication 1 Title');
+    const newsLink = await page.getByText('News 1 Title');
+
+    await expect(homePageLink)
+      .toBeVisible();
+
+    await expect(publicationLink)
+      .toBeVisible();
+
+    await expect(newsLink)
+      .toBeVisible();
   });
 
-  test('shows available links correctly', async ({
+  test('shows available links in overlay context', async ({
     page,
   }) => {
+    await page.goto('http://localhost:3000/admin/collections/publicationDetail/create');
+    await page.waitForLoadState('load');
+
+    const addContentBlock = await page.getByText('Content Block hinzufügen');
+
+    await addContentBlock.click();
+
+    const rteField = await page.locator('#field-contentBlocks .ContentEditable__root');
+
+    await rteField.fill('foo');
+    await rteField.selectText();
+
+    const linkButton = await page.locator('.toolbar-popup__button-link');
+
+    await linkButton.click();
+
+    const linkTargetInput = await page.getByLabel('Link Target');
+
     await linkTargetInput.click();
 
     const homePageLink = await page.getByText('Home Page');
