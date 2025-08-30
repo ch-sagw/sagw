b608fa3caa6667c0005a2c2594c5b7204f8a0012
Yves Tscherry <info@feerglas.com>
2025-08-30 20:25:02 +0200

    chore: use camelCase for slug

--- src/collections/Plc/People.ts
+++ src/collections/Plc/People.ts
@@ -11,7 +11,7 @@ export const People: CollectionConfig = {
   },
   fields: [
     {
-      name: 'person-department',
+      name: 'personDepartment',
       options: [
         {
           label: 'Adminstration',
