4399a63f0cfba0ab8652cde5e4add61dcb847626
Yves Tscherry <info@feerglas.com>
2025-08-31 09:47:30 +0200

    fix: refine versions config

--- src/field-templates/versions.ts
+++ src/field-templates/versions.ts
@@ -1,14 +1,11 @@
 import { IncomingCollectionVersions } from 'node_modules/payload/dist/versions/types';
 
 export const versions: IncomingCollectionVersions = {
-  // drafts: {
-  //   autosave: {
-  //     interval: 100,
-  //   },
-  //   schedulePublish: true,
-  // },
-  // maxPerDoc: 50,
   drafts: {
-    autosave: true,
+    autosave: {
+      interval: 1000,
+    },
+    schedulePublish: false,
   },
+  maxPerDoc: 50,
 };
