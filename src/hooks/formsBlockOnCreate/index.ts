7a0a0fbed4ab061dead4ab8c2a05e261de0fd22b
Yves Tscherry <info@feerglas.com>
2025-09-05 11:15:25 +0200

    feat: add forms block on newsdetail page creation

---
+++ src/hooks/formsBlockOnCreate/index.ts
@@ -0,0 +1,28 @@
+import {
+  Block, CollectionBeforeValidateHook,
+} from 'payload';
+
+export const hookFormsBlockOnCreate: CollectionBeforeValidateHook = ({
+  data, operation,
+}) => {
+
+  // only on new documents
+  if (operation === 'create') {
+    console.log(data);
+
+    if (data && !data?.content) {
+      data.content = [];
+    }
+
+    const hasFormBlock = data?.content.some((block: Block) => block.slug === 'formBlock');
+
+    if (!hasFormBlock) {
+      data?.content.push({
+        blockType: 'formBlock',
+        form: null,
+      });
+    }
+  }
+
+  return data;
+};
