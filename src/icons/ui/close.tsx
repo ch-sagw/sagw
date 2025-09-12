3e07a8a421ce1ce838133741665a9d2159824993
Yves Tscherry <info@feerglas.com>
2025-09-12 14:44:33 +0200

    chore: remove stroke and fill colors

--- src/icons/ui/close.tsx
+++ src/icons/ui/close.tsx
@@ -2,8 +2,8 @@ import { JSX } from 'react';
 
 const icon = (): JSX.Element => (
   <svg width='17' height='16' viewBox='0 0 17 16' fill='none' xmlns='http://www.w3.org/2000/svg'>
-    <path d='M13 3.5L4 12.5' stroke='#1E3241' stroke-width='1.5' stroke-linecap='square' stroke-linejoin='round' />
-    <path d='M13 12.5L4 3.5' stroke='#1E3241' stroke-width='1.5' stroke-linecap='square' stroke-linejoin='round' />
+    <path d='M13 3.5L4 12.5' stroke-width='1.5' stroke-linecap='square' stroke-linejoin='round' />
+    <path d='M13 12.5L4 3.5' stroke-width='1.5' stroke-linecap='square' stroke-linejoin='round' />
   </svg>
 );
 
