45348dd76530fdeee4334d0ebb04a19581c08a2b
Yves Tscherry <info@feerglas.com>
2025-09-14 17:16:15 +0200

    docs: define backgrounds

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,10 +2,27 @@ import type { Preview } from '@storybook/nextjs-vite';
 import '@/styles/_global.scss';
 
 const preview: Preview = {
+  initialGlobals: {
+    backgrounds: {
+      value: 'light',
+    },
+  },
   parameters: {
+    backgrounds: {
+      options: {
+        dark: {
+          name: 'SAGW Dark',
+          value: '#1E3241',
+        },
+        light: {
+          name: 'SAGW Light',
+          value: '#FFF',
+        },
+      },
+    },
     controls: {
       matchers: {
-        color: /(?<temp1>background|color)$/iu,
+        // color: /(?<temp1>background|color)$/iu,
         date: /Date$/iu,
       },
     },
