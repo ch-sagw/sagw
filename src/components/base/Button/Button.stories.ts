27ba5a8b49af36869f6068a0d536ad8b83dacba4
Marius Bleuer <m.bleuer@lycos.com>
2025-09-17 15:09:45 +0200

    feat: working on button variants

--- src/components/base/Button/Button.stories.ts
+++ src/components/base/Button/Button.stories.ts
@@ -1,4 +1,5 @@
 import type {
+  ArgTypes,
   Meta,
   StoryObj,
 } from '@storybook/nextjs-vite';
@@ -11,10 +12,68 @@ type StrictStory = StoryObj<typeof Button> & {
   args: ButtonProps;
 };
 
+const argTypes: Partial<ArgTypes<ButtonProps>> = {
+  colorTheme: {
+    control: {
+      type: 'radio',
+    },
+    options: [
+      'light',
+      'dark',
+    ],
+  },
+  iconInlineEnd: {
+    control: {
+      type: 'text',
+    },
+  },
+  iconInlineStart: {
+    control: {
+      type: 'text',
+    },
+  },
+  style: {
+    control: {
+      type: 'radio',
+    },
+    options: [
+      'filled',
+      'outlined',
+      'text',
+    ],
+  },
+  target: {
+    control: {
+      type: 'radio',
+    },
+    options: [
+      '_self',
+      '_blank',
+    ],
+  },
+  type: {
+    control: {
+      type: 'select',
+    },
+    options: [
+      'button',
+      'reset',
+      'submit',
+      'link',
+    ],
+  },
+};
+
 const meta: Meta<typeof Button> = {
+  argTypes,
   args: {},
   component: Button,
   decorators: [defaultDecorator],
+  globals: {
+    backgrounds: {
+      value: 'light',
+    },
+  },
   parameters: {/* layout: 'centered', */ },
   tags: [
     'autodocs',
@@ -26,9 +85,138 @@ const meta: Meta<typeof Button> = {
 
 export default meta;
 
-export const SampleButton: StrictStory = {
+export const ButtonPrimaryFilled: StrictStory = {
   args: {
+    ariaHasPopUp: false,
+    colorTheme: 'light',
+    iconInlineEnd: '',
+    iconInlineStart: '',
+    style: 'filled',
     text: 'Sample Button text',
+    type: 'button',
+  },
+};
+
+export const ButtonPrimaryFilledDark: StrictStory = {
+  args: {
+    ariaHasPopUp: false,
+    colorTheme: 'dark',
+    iconInlineEnd: '',
+    iconInlineStart: '',
+    style: 'filled',
+    text: 'Sample Button text',
+    type: 'button',
+  },
+  globals: {
+    backgrounds: {
+      value: 'dark',
+    },
+  },
+};
+
+export const ButtonPrimaryFilledIconStart: StrictStory = {
+  args: {
+    ariaHasPopUp: false,
+    colorTheme: 'light',
+    iconInlineEnd: '',
+    iconInlineStart: 'menu',
+    style: 'filled',
+    text: 'Sample Button text',
+    type: 'button',
+  },
+};
+
+export const ButtonPrimaryFilledIconStartDark: StrictStory = {
+  args: {
+    ariaHasPopUp: false,
+    colorTheme: 'dark',
+    iconInlineEnd: '',
+    iconInlineStart: 'menu',
+    style: 'filled',
+    text: 'Sample Button text',
+    type: 'button',
+  },
+  globals: {
+    backgrounds: {
+      value: 'dark',
+    },
+  },
+};
+
+export const ButtonPrimaryOutlined: StrictStory = {
+  args: {
+    ariaHasPopUp: false,
+    colorTheme: 'light',
+    iconInlineEnd: '',
+    iconInlineStart: 'menu',
+    style: 'outlined',
+    text: 'Sample Button text',
+    type: 'button',
+  },
+};
+
+export const ButtonPrimaryOutlinedDark: StrictStory = {
+  args: {
+    ariaHasPopUp: false,
+    colorTheme: 'dark',
+    iconInlineEnd: '',
+    iconInlineStart: 'menu',
+    style: 'outlined',
+    text: 'Sample Button text',
+    type: 'button',
+  },
+  globals: {
+    backgrounds: {
+      value: 'dark',
+    },
+  },
+};
+
+export const ButtonText: StrictStory = {
+  args: {
+    ariaHasPopUp: false,
+    colorTheme: 'light',
+    iconInlineEnd: '',
+    iconInlineStart: 'menu',
+    style: 'text',
+    text: 'Sample Button text',
+    type: 'button',
+  },
+};
+
+export const ButtonLinkPrimaryFilled: StrictStory = {
+  args: {
+    ariaHasPopUp: false,
+    colorTheme: 'light',
+    href: 'https://www.sagw.ch.com',
+    style: 'filled',
+    target: '_blank',
+    text: 'Sample Button text',
+    type: 'link',
+  },
+};
+
+export const ButtonLinkPrimaryOutlined: StrictStory = {
+  args: {
+    ariaHasPopUp: false,
+    colorTheme: 'light',
+    href: 'https://www.sagw.ch.com',
+    style: 'outlined',
+    target: '_blank',
+    text: 'Sample Button text',
+    type: 'link',
+  },
+};
+
+export const ButtonLinkText: StrictStory = {
+  args: {
+    ariaHasPopUp: false,
+    colorTheme: 'light',
+    href: 'https://www.sagw.ch.com',
+    style: 'text',
+    target: '_blank',
+    text: 'Sample Button text',
+    type: 'link',
   },
 };
 
--- src/components/base/Button/Button.tsx
+++ src/components/base/Button/Button.tsx
@@ -1,18 +1,118 @@
-import 'server-only';
 import React from 'react';
+import { cva } from 'cva';
 import styles from '@/components/base/Button/Button.module.scss';
+import { Icon } from '@/icons';
+import Link from 'next/link';
 
-export type InterfaceButtonPropTypes = {
+type BaseProps = {
+  colorTheme: 'light' | 'dark';
+  iconInlineStart?: string;
+  iconInlineEnd?: string;
+  type: 'button' | 'link';
+  style: 'filled' | 'outlined' | 'text';
   text: string;
 };
 
-export const Button = ({
-  text,
-}: InterfaceButtonPropTypes): React.JSX.Element => (
-  <button
-    className={styles.button}
-    data-testid='button'
-  >
-    {text}
-  </button>
-);
+type ButtonProps = BaseProps & {
+  ariaHasPopUp: boolean,
+};
+
+type ButtonLinkProps = BaseProps & {
+  href: string;
+  target: '_blank' | '_self';
+};
+
+export type InterfaceButtonPropTypes = ButtonProps | ButtonLinkProps;
+
+const classes = cva([styles.button], {
+  variants: {
+    colorTheme: {
+      dark: [styles.dark],
+      light: [styles.light],
+    },
+    style: {
+      filled: [styles.buttonFilled],
+      iconEnd: [styles.iconEnd],
+      iconStart: [styles.iconStart],
+      innerText: [styles.innerText],
+      outlined: [styles.buttonOutlined],
+      text: [styles.buttonText],
+    },
+  },
+});
+
+// TODOs
+// - Integrate tracking
+// - Add support for disabled state
+// - Add support for loading state
+// - Add support for visually hidden text for target _blank
+
+export const Button = (props: InterfaceButtonPropTypes): React.JSX.Element => {
+  const {
+    colorTheme,
+    iconInlineEnd,
+    iconInlineStart,
+    type,
+    style,
+    text,
+  } = props;
+
+  // Render the NextJS link element
+  if (type === 'link') {
+    if ('href' in props && 'target' in props) {
+      const {
+        href,
+        target,
+      } = props;
+
+      return (
+        <Link
+          className={classes({
+            colorTheme,
+            style,
+          })}
+          data-testid='link'
+          href={href}
+          target={target}
+        >
+          {iconInlineStart && (
+            <span className={styles.iconStart}>
+              <Icon name={iconInlineStart} />
+            </span>
+          )}
+          <span className={styles.innerText}>{text}</span>
+          {iconInlineEnd && (
+            <span className={styles.iconEnd}>
+              <Icon name={iconInlineEnd} />
+            </span>
+          )
+          }
+        </Link >
+      );
+    }
+  }
+
+  // Render a proper button
+  return (
+    <button
+      className={classes({
+        colorTheme,
+        style,
+      })}
+      data-testid='button'
+      type='button'
+    >
+      {iconInlineStart && (
+        <span className={styles.iconStart}>
+          <Icon name={iconInlineStart} />
+        </span>
+      )}
+      <span className={styles.innerText}>{text}</span>
+      {iconInlineEnd && (
+        <span className={styles.iconEnd}>
+          <Icon name={iconInlineEnd} />
+        </span>
+      )}
+    </button>
+  );
+};
