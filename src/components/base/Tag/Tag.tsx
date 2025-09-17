b3fa1d31820b566d489628f6d3162c197c972fde
Yves Tscherry <info@feerglas.com>
2025-09-17 08:20:13 +0200

    feat: add tag component

---
+++ src/components/base/Tag/Tag.tsx
@@ -0,0 +1,36 @@
+import 'server-only';
+import React from 'react';
+import { cva } from 'cva';
+import styles from '@/components/base/Tag/Tag.module.scss';
+
+export type InterfaceTagPropTypes = {
+  text: string;
+  colorTheme: 'primary' | 'secondary';
+  className?: string;
+};
+
+export const Tag = ({
+  text,
+  colorTheme,
+  className,
+}: InterfaceTagPropTypes): React.JSX.Element => {
+  const classes = cva([
+    styles.tag,
+    className,
+  ], {
+    variants: {
+      colorTheme: {
+        primary: [styles.primary],
+        secondary: [styles.secondary],
+      },
+    },
+  });
+
+  return (
+    <span
+      className={classes({
+        colorTheme,
+      })}
+    >{text}</span>
+  );
+};
