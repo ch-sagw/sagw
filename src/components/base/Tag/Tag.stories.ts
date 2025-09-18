7f78d4afdfdd671b4736f49f2210d1a6be5bc9ca
Yves Tscherry <info@feerglas.com>
2025-09-18 09:24:11 +0200

    Feat/fe component tag (#400)
    feat: add tag component

---
+++ src/components/base/Tag/Tag.stories.ts
@@ -0,0 +1,41 @@
+import type {
+  Meta,
+  StoryObj,
+} from '@storybook/nextjs-vite';
+import { Tag } from '@/components/base/Tag/Tag';
+import { defaultDecorator } from '@/storybook-helpers';
+
+type TagProps = React.ComponentProps<typeof Tag>;
+
+type StrictStory = StoryObj<typeof Tag> & {
+  args: TagProps;
+};
+
+const meta: Meta<typeof Tag> = {
+  args: {},
+  component: Tag,
+  decorators: [defaultDecorator],
+  parameters: {/* layout: 'centered', */ },
+  tags: [
+    'autodocs',
+    'visual:check',
+    'a11y:check',
+  ],
+  title: 'Components/base/Tag',
+};
+
+export default meta;
+
+export const TagPrimary: StrictStory = {
+  args: {
+    colorTheme: 'primary',
+    text: 'Some text for the tag',
+  },
+};
+
+export const TagSecondary: StrictStory = {
+  args: {
+    colorTheme: 'secondary',
+    text: 'Some text for the tag',
+  },
+};
