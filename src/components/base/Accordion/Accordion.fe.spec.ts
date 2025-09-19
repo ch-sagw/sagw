fd6d2bbe663a93a74741dcf91c378a3429e13ec9
Yves Tscherry <info@feerglas.com>
2025-09-19 11:08:31 +0200

    feat: add accordion component

---
+++ src/components/base/Accordion/Accordion.fe.spec.ts
@@ -0,0 +1,17 @@
+import {
+  expect,
+  test,
+} from '@playwright/test';
+import { navigate } from '@/automated-testing/helpers';
+
+test('component should be visible', async ({
+  page,
+}) => {
+  // Put the story name in here. Get it from the url in storybook...
+  await navigate(page, 'components-base-accordion--sample-story');
+
+  const elem = await page.getByText('some text');
+
+  await expect(elem)
+    .toBeVisible();
+});
---
+++ src/components/base/Accordion/Accordion.tsx
@@ -0,0 +1,84 @@
+import 'server-only';
+import React, { useState } from 'react';
+import { cva } from 'cva';
+import styles from '@/components/base/Accordion/Accordion.module.scss';
+import { Icon } from '@/icons';
+import {
+  InterfaceRtePropTypes, Rte,
+} from '@/components/base/Rte/Rte';
+
+interface InterfaceAccordionItem {
+  title: string;
+  content: InterfaceRtePropTypes['text'];
+}
+
+export type InterfaceAccordionPropTypes = {
+  items: InterfaceAccordionItem[];
+  titleLevel: 2 | 3 | 4 | 5;
+};
+
+const accordionItemClasses = cva([styles.item], {
+  variants: {
+    active: {
+      false: '',
+      true: styles.active,
+    },
+  },
+});
+
+export const Accordion = ({
+  items,
+  titleLevel,
+}: InterfaceAccordionPropTypes): React.JSX.Element => {
+  const [
+    activeAccordion,
+    setActiveAccordion,
+  ] = useState<number | undefined>(undefined);
+  const HeadingElem: React.ElementType = `h${titleLevel}`;
+
+  const onClick = (id: number): void => {
+    if (id === activeAccordion) {
+      setActiveAccordion(undefined);
+    } else {
+      setActiveAccordion(id);
+    }
+  };
+
+  return (
+    <div className={styles.accordion}>
+      {items.map((item, key) => (
+        <div
+          key={key}
+          className={accordionItemClasses({
+            active: key === activeAccordion,
+          })}
+        >
+          <HeadingElem className={styles.title}>
+            <button
+              className={styles.button}
+              onClick={() => {
+                onClick(key);
+              }}
+            >
+              <span className={styles.buttonText}>{item.title}</span>
+              <Icon
+                name='plus'
+                className={styles.icon}
+              />
+            </button>
+          </HeadingElem>
+
+          <section className={styles.content}>
+            <Rte
+              className={styles.rte}
+              text={item.content}
+              rteConfig='rte2'
+            />
+          </section>
+
+        </div>
+      ))}
+    </div>
+
+  );
+};
