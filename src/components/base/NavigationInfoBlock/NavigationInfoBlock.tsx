5e44253de362d6374cf6490368d853bfcda41260
Yves Tscherry <info@feerglas.com>
2025-09-22 17:50:59 +0200

    feat: add className prop

--- src/components/base/NavigationInfoBlock/NavigationInfoBlock.tsx
+++ src/components/base/NavigationInfoBlock/NavigationInfoBlock.tsx
@@ -5,18 +5,20 @@ import styles from '@/components/base/NavigationInfoBlock/NavigationInfoBlock.mo
 export type InterfaceNavigationInfoBlockPropTypes = {
   title: string;
   text: string;
+  className?: string;
 };
 
 export const NavigationInfoBlock = ({
   title,
   text,
+  className,
 }: InterfaceNavigationInfoBlockPropTypes): React.JSX.Element => (
 
   // Hidden for screenreaders. NavItems will contain this as hidden text.
   <div
     aria-hidden={true}
     role='presentation'
-    className={styles.infoBlock}
+    className={`${styles.infoBlock} ${className}`}
   >
     <p className={styles.title}>{title}</p>
     <p className={styles.text}>{text}</p>
