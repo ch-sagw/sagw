1aaae45b638d2ae16919873e3d74f8f0a5b44cc6
Yves Tscherry <info@feerglas.com>
2025-10-16 08:01:51 +0200

    feat: add sample structured data to test

--- src/components/base/FooterContact/FooterContact.tsx
+++ src/components/base/FooterContact/FooterContact.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { cva } from 'cva';
 import styles from '@/components/base/FooterContact/FooterContact.module.scss';
 import { InterfaceFooterContact } from '@/payload-types';
 
 export type InterfaceFooterContactPropTypes = {
-  sampleProperty: string;
   context: 'sampleContext'
 } & InterfaceFooterContact;
 
@@ -18,11 +17,56 @@ const sampleClasses = cva([styles.baseStyle], {
 
 export const FooterContact = ({
   context,
-  sampleProperty,
-}: InterfaceFooterContactPropTypes): React.JSX.Element => (
-  <div
-    className={sampleClasses({
-      context: context ?? undefined,
-    })}
-  >{sampleProperty}</div>
-);
+}: InterfaceFooterContactPropTypes): React.JSX.Element => {
+  const structuredData = {
+    '@context': 'https://schema.org',
+    '@type': 'Organization',
+    'address': {
+      '@type': 'PostalAddress',
+      'addressCountry': 'CH',
+      'addressLocality': 'Bern',
+      'postalCode': '3001',
+      'streetAddress': 'Laupenstrasse 7, Postfach',
+    },
+    'email': 'sagw@sagw.ch',
+    'name': 'SAGW Schweizerische Akademie der Geistes- und Sozialwissenschaften',
+    'telephone': '+41 31 306 92 50',
+    'url': 'https://sagw.ch',
+  };
+
+  return (
+    <Fragment>
+      <script
+        type='application/ld+json'
+        dangerouslySetInnerHTML={{
+          /* eslint-disable @typescript-eslint/naming-convention */
+          __html: JSON.stringify(structuredData),
+          /* eslint-enable @typescript-eslint/naming-convention */
+        }}
+      />
+
+      <div
+        className={sampleClasses({
+          context: context ?? undefined,
+        })}
+      >
+        {/* Name */}
+        SAGW Schweizerische Akademie der Geistes- und Sozialwissenschaften
+
+        {/* Address */}
+        Haus der Akademien
+        Laupenstrasse 7
+        Postfach
+
+        {/* ZIP and City */}
+        CH-3001 Bern
+
+        {/* Phone */}
+        +41 31 306 92 50
+
+        {/* Mail */}
+        sagw@sagw.ch
+      </div>
+    </Fragment>
+  );
+};
