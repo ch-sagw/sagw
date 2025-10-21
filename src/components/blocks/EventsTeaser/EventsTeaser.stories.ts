70c00eaf17b935bfbbb4a058ea03d033986046e5
Yves Tscherry <info@feerglas.com>
2025-10-21 11:20:05 +0200

    fix: correctly define stories

--- src/components/blocks/EventsTeaser/EventsTeaser.stories.ts
+++ src/components/blocks/EventsTeaser/EventsTeaser.stories.ts
@@ -2,27 +2,22 @@ import type {
   Meta,
   StoryObj,
 } from '@storybook/nextjs-vite';
-import { EventsTeaser } from '@/components/blocks/EventsTeaser/EventsTeaser';
+import { EventsTeaserComponent } from '@/components/blocks/EventsTeaser/EventsTeaser.component';
 import { defaultDecoratorNoPadding } from '@/storybook-helpers';
 import {
   DateNotSameMonth, DateNotSameYear, Full,
 } from '@/components/base/EventsListItem/EventsListItem.stories';
 
-type EventsTeaserProps = React.ComponentProps<typeof EventsTeaser>;
+type EventsTeaserProps = React.ComponentProps<typeof EventsTeaserComponent>;
 
-type StrictStory = StoryObj<typeof EventsTeaser> & {
+type StrictStory = StoryObj<typeof EventsTeaserComponent> & {
   args: EventsTeaserProps;
 };
 
-const meta: Meta<typeof EventsTeaser> = {
+const meta: Meta<typeof EventsTeaserComponent> = {
   args: {},
-  component: EventsTeaser,
+  component: EventsTeaserComponent,
   decorators: [defaultDecoratorNoPadding],
-  globals: {
-    backgrounds: {
-      value: 'white',
-    },
-  },
   parameters: {
     layout: 'fullscreen',
   },
@@ -40,13 +35,13 @@ export const DefaultTeaser: StrictStory = {
   args: {
     allLink: {
       href: 'https://foo.bar',
-      text: 'Alle Events',
+      text: 'Alle News',
     },
     items: [
       DateNotSameMonth.args,
       DateNotSameYear.args,
       Full.args,
     ],
-    title: 'Events',
+    title: 'News',
   },
 };
