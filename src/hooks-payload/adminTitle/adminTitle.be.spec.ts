078ebd811be3406d223a0817aed192b41fb7c691
Yves Tscherry <info@feerglas.com>
2025-10-22 16:56:04 +0200

    test: add test for admin title for event detail pages

--- src/hooks-payload/adminTitle/adminTitle.be.spec.ts
+++ src/hooks-payload/adminTitle/adminTitle.be.spec.ts
@@ -53,6 +53,58 @@ test.describe('adminTitle', () => {
       .toBe('News Detail Page Hero Title');
   });
 
+  test('correctly adopts adminTitle from event details (event detail page)', async ({
+    page,
+  }) => {
+    // create a news detail page
+    await page.goto('http://localhost:3000/admin/collections/eventDetailPage/create');
+    await page.waitForLoadState('networkidle');
+
+    const eventTitle = await page.locator('#field-eventDetails .render-fields .rich-text-lexical')
+      .first()
+      .locator('.ContentEditable__root');
+    const dateField = await page.locator('#field-eventDetails__date input');
+    const externalLinkText = await page.locator('#field-link .render-fields .rich-text-lexical')
+      .nth(1)
+      .locator('.ContentEditable__root');
+    const externalLink = await page.locator('#field-link__externalLink');
+
+    await eventTitle.fill('foo bar$ baz');
+    await dateField.fill('2025-08-31');
+    await externalLinkText.fill('some event');
+    await externalLink.fill('https://www.foo.bar');
+
+    // save
+    const saveButton = await page.getByRole('button', {
+      name: 'Änderungen veröffentlichen',
+    });
+
+    await saveButton.click();
+
+    // wait for confirmation toast and close it
+    const closeToast = await page.locator('.payload-toast-container [data-close-button="true"]');
+
+    await closeToast.click();
+
+    // wait for refresh
+    await page.waitForURL(/http:\/\/localhost:3000\/admin\/collections\/eventDetailPage\/[a-f0-9]+$/u);
+
+    const heading = page.locator('.doc-header__title.render-title');
+
+    await expect(heading)
+      .toHaveText('foo bar$ baz');
+
+    // test api response
+    const url = page.url();
+    const parts = url.split('/');
+    const id = parts[parts.length - 1];
+    const res = await fetch(`http://localhost:3000/api/eventDetailPage/${id}`);
+    const newsPage = await res.json();
+
+    await expect(newsPage.adminTitle)
+      .toBe('foo bar$ baz');
+  });
+
   test('correctly transforms lexical to plaintext', async ({
     page,
   }) => {
