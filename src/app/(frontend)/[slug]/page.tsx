949e37037882637f712bd51a4af8fb2db03618f3
Yves Tscherry <info@feerglas.com>
2025-10-22 11:41:22 +0200

    chore: remove header from test pages

--- src/app/(frontend)/[slug]/page.tsx
+++ src/app/(frontend)/[slug]/page.tsx
@@ -5,8 +5,6 @@ import configPromise from '@/payload.config';
 import { Config } from '@/payload-types';
 import { RenderBlocks } from '@/app/(frontend)/RenderBlocks';
 import { getTenant } from '@/app/providers/TenantProvider.server';
-import { Header } from '@/components/global/Header/Header';
-import { ColorMode } from '@/components/base/types/colorMode';
 
 export const revalidate = 0;
 
@@ -103,63 +101,8 @@ export default async function DetailPage({
     return <p>No page data</p>;
   }
 
-  // Get header data (same as home page)
-  const headerData = await payload.find({
-    collection: 'header',
-    depth: 1,
-    limit: 1,
-    locale: lang as Config['locale'],
-    overrideAccess: false,
-    where: {
-      tenant: {
-        equals: tenant,
-      },
-    },
-  });
-
-  if (!headerData.docs || headerData.docs.length < 1) {
-    return <p>No header data</p>;
-  }
-
-  const navData = headerData.docs[0].navigation;
-
-  if (!navData || navData.navItems.length < 1) {
-    return <p>No nav items in header data</p>;
-  }
-
-  const langnavData = headerData.docs[0].languageNavigation;
-
-  if (!langnavData || !langnavData.description || !langnavData.title) {
-    return <p>No lang nav data in header data</p>;
-  }
-
-  const metanavData = headerData.docs[0].metanavigation;
-
-  if (!metanavData?.metaLinks || metanavData.metaLinks.length < 1) {
-    return <p>No metanav data in header data</p>;
-  }
-
-  const colorMode: ColorMode = 'dark';
-
-  const headerProps = {
-    colorMode,
-    currentLang: 'de',
-    langnav: langnavData,
-    logoLink: '/',
-    menuButton: {
-      close: 'Close',
-      open: 'Open',
-    },
-    metanav: metanavData,
-    navigation: navData,
-  };
-
   return (
     <Fragment>
-      <Header
-        {...headerProps}
-      />
-
       <main>
         <div className='detail-page'>
           <RenderBlocks
