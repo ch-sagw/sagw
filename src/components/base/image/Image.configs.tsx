fb8d4b8488789157c7ab6949a69a045c2c297061
Marius Bleuer <m.bleuer@lycos.com>
2025-10-24 17:46:37 +0200

    feat: working on image variants

--- src/components/base/image/Image.configs.tsx
+++ src/components/base/image/Image.configs.tsx
@@ -10,6 +10,19 @@ export const getSrcSet = ({
   let srcSet = '';
 
   switch (variant) {
+    case 'hero':
+      srcSet = `
+        ${src}?${params}&w=2500&h=1406&q=60 2500w,
+        ${src}?${params}&w=1000&h=562&dpr=2&q=30 2000w,
+        ${src}?${params}&w=1600&h=900&q=60 1600w,
+        ${src}?${params}&w=600&h=337&dpr=2&q=30 1200w,
+        ${src}?${params}&w=1000&h=562&q=60 1000w,
+        ${src}?${params}&w=350&h=197&dpr=2&q=30 700w,
+        ${src}?${params}&w=600&h=337&q=60 600w,
+        ${src}?${params}&w=350&h=197&q=60 350w,
+      `;
+      break;
+
     case 'portrait':
       srcSet = `
         ${src}?${params}&w=600&h=600&dpr=2&q=30 1200w,
@@ -40,8 +53,6 @@ export const getSrcSet = ({
     // Fallback logic for unknown variants
   }
 
-  console.log(srcSet.trim());
-
   return srcSet.trim();
 
 };
@@ -50,6 +61,16 @@ export const getSizes = (variant: string): string => {
   let sizes = '';
 
   switch (variant) {
+    case 'hero':
+      sizes = `
+        (min-width: 100rem) 2500px,
+        (min-width: 80rem) 1600px,
+        (min-width: 40rem) 1000px,
+        (min-width: 22.5rem) 600px,
+        350px
+      `;
+      break;
+
     case 'portrait':
       sizes = '(min-width: 64rem) 600px, 400px';
       break;
@@ -67,5 +88,5 @@ export const getSizes = (variant: string): string => {
     // Fallback logic for unknown variants
   }
 
-  return sizes;
+  return sizes.trim();
 };
--- src/components/base/image/image.stories.ts
+++ src/components/base/image/image.stories.ts
@@ -26,6 +26,20 @@ const meta: Meta<typeof Image> = {
 
 export default meta;
 
+export const MagazineHero: StrictStory = {
+  args: {
+    alt: 'Das Bild zeigt eine junge Frau, mit blonden schulterlangen Haaren und Brille',
+    focalPointX: 0.5,
+    focalPointY: 0.5,
+    height: 675,
+    loading: 'eager',
+    performanceMark: 'magazine-hero-teaser',
+    src: 'https://sagw-nu.gumlet.io/api/images/file/mbl_1_9417372470_920fe62811_o.jpg',
+    variant: 'hero',
+    width: 1200,
+  },
+};
+
 export const Portrait: StrictStory = {
   args: {
     alt: 'Das Bild zeigt eine junge Frau, mit blonden schulterlangen Haaren und Brille',
@@ -33,7 +47,7 @@ export const Portrait: StrictStory = {
     focalPointY: 0.5,
     height: 600,
     loading: 'eager',
-    performanceMark: 'Portrait',
+    performanceMark: 'portrait',
     src: 'https://sagw-nu.gumlet.io/api/images/file/mbl_1_9417372470_920fe62811_o.jpg',
     variant: 'portrait',
     width: 600,
@@ -61,7 +75,7 @@ export const PublicationTeaser: StrictStory = {
     focalPointY: 0.5,
     height: 114,
     loading: 'lazy',
-    performanceMark: 'Portrait',
+    performanceMark: '',
     src: 'https://sagw-nu.gumlet.io/api/images/file/mbl_1_9417372470_920fe62811_o.jpg',
     variant: 'publicationTeaser',
     width: 80,
