e42ecbbbf9c10b29e1d9dc87aa33a32431e7dee9
Marius Bleuer <m.bleuer@lycos.com>
2025-10-28 16:39:56 +0100

    style: adding visual play button fixes

--- src/icons/ui/play.tsx
+++ src/icons/ui/play.tsx
@@ -2,7 +2,7 @@ import { JSX } from 'react';
 
 const icon = (): JSX.Element => (
   <svg viewBox='0 0 17 16' fill='none' xmlns='http://www.w3.org/2000/svg'>
-    <path d='M4.66675 2.83325V13.4999L14.0001 8.16659L4.66675 2.83325Z' strokeWidth='1.5' strokeMiterlimit='10' strokeLinecap='square' />
+    <path d='M4.66675 2.83325V13.4999L14.0001 8.16659L4.66675 2.83325Z' />
   </svg>
 );
 
