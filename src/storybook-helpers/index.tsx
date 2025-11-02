7f4907b819f87290b37b16326a4d65d3ccb7700a
Yves Tscherry <info@feerglas.com>
2025-11-02 23:42:03 +0100

    test: fix fe tests with non-relative positioned elements

--- src/storybook-helpers/index.tsx
+++ src/storybook-helpers/index.tsx
@@ -20,7 +20,9 @@ export const defaultDecoratorNoPadding = (Story: PartialStoryFn): ReactElement =
   <div
     style={{
       display: 'block',
-      padding: '0.1px',
+      minHeight: '1px',
+      minWidth: '1px',
+      padding: '0',
     }}
     data-testid={vrtConfig.testid}
     id={vrtConfig.testid}
