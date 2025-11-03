2d32c604362f9f4c5478bfc7ed06c0bad3e8ac57
Yves Tscherry <info@feerglas.com>
2025-11-03 17:09:40 +0100

    feat: add hero component with markup (unstyled)

---
+++ src/components/global/Hero/Hero.stories.ts
@@ -0,0 +1,125 @@
+import type {
+  Meta,
+  StoryObj,
+} from '@storybook/nextjs-vite';
+import { Hero } from '@/components/global/Hero/Hero';
+import { defaultDecorator } from '@/storybook-helpers';
+import { simpleRteConfig } from '@/utilities/simpleRteConfig';
+
+type HeroProps = React.ComponentProps<typeof Hero>;
+
+type StrictStory = StoryObj<typeof Hero> & {
+  args: HeroProps;
+};
+
+const meta: Meta<typeof Hero> = {
+  args: {},
+  component: Hero,
+  decorators: [defaultDecorator],
+  parameters: {/* layout: 'centered', */ },
+  tags: [
+    'autodocs',
+    'visual:check',
+    'a11y:check',
+  ],
+  title: 'Components/global/Hero',
+};
+
+export default meta;
+
+export const Home: StrictStory = {
+  args: {
+    optionalLink: {
+      includeLink: true,
+      link: {
+        internalLink: 'someId',
+        linkText: simpleRteConfig('Über uns'),
+      },
+    },
+    pageLanguage: 'de',
+    sideTitle: simpleRteConfig('Die Schweizer Akademie der Geistes- und Sozialwissenschaften'),
+    title: simpleRteConfig('Wir fördern langfristige Forschungsinfrastrukturen, vernetzen Akteure und vermitteln Wissen.'),
+    type: 'home',
+  },
+};
+
+export const MagazineDetail: StrictStory = {
+  args: {
+    author: simpleRteConfig('Lea Haller'),
+    breadcrumb: {
+      colorMode: 'white',
+      items: [
+        {
+          link: 'https://www.foo.bar',
+          text: 'Aktivitäten',
+        },
+        {
+          link: 'https://www.foo.bar',
+          text: 'Magazin',
+        },
+      ],
+      pageLanguage: 'de',
+    },
+    colorMode: 'white',
+    date: '2025-08-31T12:00:00.000Z',
+    exportArticleText: simpleRteConfig('Artikel exportieren'),
+    lead: simpleRteConfig('Intelligenz und Dummheit sind als Konzepte eng miteinander verwoben. Als man die Vernunft als Motor des Fortschritts anzusehen begann, geriet auch die Dummheit verstärkt ins Visier. Dass sie bis heute nicht ausgerottet werden konnte, gehört zu den wohl grössten Kränkungen der Menschheit. Klar ist: Dumm sind in der Regel die anderen.'),
+    pageLanguage: 'de',
+    title: simpleRteConfig('Dummheit: Die andere Seite der Medaille'),
+    type: 'magazineDetail',
+  },
+};
+
+export const NewsDetail: StrictStory = {
+  args: {
+    breadcrumb: {
+      colorMode: 'white',
+      items: [
+        {
+          link: 'https://www.foo.bar',
+          text: 'Aktivitäten',
+        },
+        {
+          link: 'https://www.foo.bar',
+          text: 'Magazin',
+        },
+      ],
+      pageLanguage: 'de',
+    },
+    colorMode: 'white',
+    date: '2025-08-31T12:00:00.000Z',
+    pageLanguage: 'de',
+    title: simpleRteConfig('„Verstehen, was ist“ - Die SAGW stellt ihre Strategie 2025-2028 vor'),
+    type: 'newsDetail',
+  },
+};
+
+export const EventDetail: StrictStory = {
+  args: {
+    breadcrumb: {
+      colorMode: 'white',
+      items: [
+        {
+          link: 'https://www.foo.bar',
+          text: 'Aktivitäten',
+        },
+        {
+          link: 'https://www.foo.bar',
+          text: 'Magazin',
+        },
+      ],
+      pageLanguage: 'de',
+    },
+    colorMode: 'white',
+    eventDetails: {
+      dateEnd: '2025-09-01T12:00:00.000Z',
+      dateStart: '2025-08-31T12:00:00.000Z',
+      eventLocation: 'Basel',
+      language: 'In English',
+    },
+    pageLanguage: 'de',
+    tag: 'Konferenz',
+    title: simpleRteConfig('SGAS/SSEA & VAD Conference: African Perspectives on Global Transformations'),
+    type: 'eventDetail',
+  },
+};
---
+++ src/components/global/Hero/Hero.tsx
@@ -0,0 +1,183 @@
+import React from 'react';
+import { cva } from 'cva';
+import styles from '@/components/global/Hero/Hero.module.scss';
+import {
+  Config,
+  InterfaceHeroField, InterfaceHeroFieldHome, InterfaceHeroFieldMagazineDetail, InterfaceHeroFieldNewsDetail,
+} from '@/payload-types';
+import {
+  Breadcrumb, InterfaceBreadcrumbPropTypes,
+} from '@/components/base/Breadcrumb/Breadcrumb';
+import { InterfaceRte } from '@/components/base/types/rte';
+import { rteToHtml } from '@/utilities/rteToHtml';
+import { SafeHtml } from '@/components/base/SafeHtml/SafeHtml';
+import { Tag } from '@/components/base/Tag/Tag';
+import { Button } from '@/components/base/Button/Button';
+import { ColorMode } from '@/components/base/types/colorMode';
+import { Icon } from '@/icons';
+import { formatEventDetails } from '@/components/base/EventsListItem/helpers';
+import { formatDateToReadableString } from '@/components/helpers/date';
+
+type BaseHeroProps = {
+  breadcrumb?: InterfaceBreadcrumbPropTypes;
+  pageLanguage: Config['locale'];
+};
+
+export type InterfaceHeroPropTypes =
+  | (BaseHeroProps & {
+    type: 'home';
+  } & InterfaceHeroFieldHome)
+  | (BaseHeroProps & {
+    type: 'generic';
+  } & InterfaceHeroField)
+  | (BaseHeroProps & {
+    exportArticleText?: InterfaceRte;
+    type: 'magazineDetail';
+  } & InterfaceHeroFieldMagazineDetail)
+  | (BaseHeroProps & {
+    type: 'newsDetail';
+  } & InterfaceHeroFieldNewsDetail)
+  | (BaseHeroProps & {
+    eventDetails?: {
+      dateStart: string;
+      dateEnd?: string;
+      language?: string;
+      time?: string;
+      eventLocation?: string;
+    };
+    tag: string;
+    type: 'eventDetail';
+  } & InterfaceHeroField);
+
+export const Hero = (props: InterfaceHeroPropTypes): React.JSX.Element => {
+  const heroClasses = cva([styles.hero], {
+    variants: {
+      colorMode: {
+        dark: [styles.dark],
+        light: [styles.light],
+        white: [styles.white],
+      },
+    },
+  });
+
+  let heroColorMode: ColorMode;
+
+  if (props.type === 'home') {
+    heroColorMode = 'dark';
+  } else {
+    heroColorMode = props.colorMode;
+  }
+
+  let eventDetailsString;
+
+  if (props.type === 'eventDetail' && props.eventDetails) {
+    eventDetailsString = formatEventDetails({
+      dateEnd: props.eventDetails.dateEnd,
+      dateStart: props.eventDetails.dateStart,
+      eventLocation: props.eventDetails.eventLocation,
+      language: props.eventDetails.language,
+      pageLanguage: props.pageLanguage,
+      time: props.eventDetails.time,
+    });
+  }
+
+  return (
+    <div className={heroClasses({
+      colorMode: heroColorMode,
+    })}>
+
+      {/* Breadcrumb */}
+      {props.breadcrumb &&
+        <Breadcrumb
+          {...props.breadcrumb}
+        />
+      }
+
+      {/* Side Title */}
+      {'sideTitle' in props && props.sideTitle &&
+        <SafeHtml
+          as='p'
+          html={rteToHtml(props.sideTitle)}
+        />
+      }
+
+      {/* Event Detail tag */}
+      {props.type === 'eventDetail' && props.tag &&
+        <Tag
+          text={props.tag}
+          colorTheme='secondary'
+        />
+      }
+
+      {/* Title */}
+      <SafeHtml
+        as='h1'
+        html={rteToHtml(props.title)}
+      />
+
+      {/* Lead */}
+      {props.lead &&
+        <SafeHtml
+          as='p'
+          html={rteToHtml(props.lead)}
+        />
+      }
+
+      {/* News detail: date */}
+      {props.type === 'newsDetail' && props.date &&
+        <p>{formatDateToReadableString({
+          dateString: props.date,
+          locale: props.pageLanguage,
+        })}</p>
+      }
+
+      {/* Event detail: event detail summary */}
+      {eventDetailsString &&
+        <SafeHtml
+          as='p'
+          html={eventDetailsString}
+        />
+      }
+
+      {/* Magazine detail: author, date & export button */}
+      {props.type === 'magazineDetail' &&
+        <div className={styles.magazineDetailExtras}>
+          <p className={styles.authorDate}>
+            <SafeHtml
+              as='span'
+              html={rteToHtml(props.author)}
+              className={styles.author}
+            />
+            <span className={styles.date}>{props.date}</span>
+          </p>
+          <Button
+            element='button'
+            text={rteToHtml(props.exportArticleText)}
+            colorMode={heroColorMode}
+            style='outlined'
+            iconInlineStart={'exportIcon' as keyof typeof Icon}
+            onClick={() => {
+
+              // TODO
+              console.log('export clicked');
+            }}
+          />
+        </div>
+      }
+
+      {/* Link */}
+      {'optionalLink' in props && props.optionalLink && props.optionalLink.includeLink && props.optionalLink.link &&
+        <Button
+          element='link'
+          href={props.optionalLink.link.internalLink}
+          text={rteToHtml(props.optionalLink.link.linkText)}
+          colorMode={heroColorMode}
+          style='text'
+          pageLanguage={props.pageLanguage}
+          iconInlineStart={'arrowLeft' as keyof typeof Icon}
+          ariaCurrent={true}
+        />
+      }
+    </div >
+  );
+};
