c6de1c4bf61b00b7ae10077d24121a28e2f6ffdb
Yves Tscherry <info@feerglas.com>
2025-12-06 16:26:49 +0100

    chore: direct return

--- src/components/blocks/HomeTeaser/HomeTeaser.tsx
+++ src/components/blocks/HomeTeaser/HomeTeaser.tsx
@@ -11,60 +11,56 @@ export type InterfaceHomeTeaserPropTypes = {} & InterfaceHomeTeasersBlock;
 
 export const HomeTeaser = ({
   homeTeasers,
-}: InterfaceHomeTeaserPropTypes): React.JSX.Element => {
-  console.log('foo');
+}: InterfaceHomeTeaserPropTypes): React.JSX.Element => (
+  <div
+    className={styles.teasers}
+  >
+    {homeTeasers?.map((teaser, index) => {
+      const {
+        category,
+        title,
+        text,
+        // iconName,
+        link,
+      } = teaser;
 
-  return (
-    <div
-      className={styles.teasers}
-    >
-      {homeTeasers?.map((teaser, index) => {
-        const {
-          category,
-          title,
-          text,
-          // iconName,
-          link,
-        } = teaser;
+      return (
+        <Section
+          key={index}
+          title={category}
+          colorMode='white'
+          subtitle={rteToHtml(title)}
+          className={styles.teaser}
+          additionalContentClassName={styles.stickyContent}
+          titleClassName={styles.stickyTitle}
+          additionalStickyContent={
+            // TODO: when moxy ready, render proper icon
+            <Icon
+              name='arrowRight'
+              className={styles.icon}
+            />
+          }
+        >
+          <SafeHtml
+            as='p'
+            html={rteToHtml(text)}
+            className={styles.text}
+          />
 
-        return (
-          <Section
-            key={index}
-            title={category}
+          <Button
+            text={rteToHtml(link.linkText)}
             colorMode='white'
-            subtitle={rteToHtml(title)}
-            className={styles.teaser}
-            additionalContentClassName={styles.stickyContent}
-            titleClassName={styles.stickyTitle}
-            additionalStickyContent={
-              // TODO: when moxy ready, render proper icon
-              <Icon
-                name='arrowRight'
-                className={styles.icon}
-              />
-            }
-          >
-            <SafeHtml
-              as='p'
-              html={rteToHtml(text)}
-              className={styles.text}
-            />
+            element='link'
+            style='text'
+            iconInlineStart={'arrowRight' as keyof typeof Icon}
+            className={styles.link}
 
-            <Button
-              text={rteToHtml(link.linkText)}
-              colorMode='white'
-              element='link'
-              style='text'
-              iconInlineStart={'arrowRight' as keyof typeof Icon}
-              className={styles.link}
+            // TODO: generate valid link
+            href={link.internalLink.documentId}
+          />
+        </Section>
+      );
+    })}
 
-              // TODO: generate valid link
-              href={link.internalLink.documentId}
-            />
-          </Section>
-        );
-      })}
-
-    </div>
-  );
-};
+  </div>
+);
