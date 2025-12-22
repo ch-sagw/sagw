58ac7a4bcf0e92288569b57685b8db26e7140dd8
Marius Bleuer <m.bleuer@lycos.com>
2025-12-22 09:28:19 +0100

    feat: adding image handling to NationalDictionariesOverview component

--- src/components/blocks/NationalDictionariesOverview/NationalDictionariesOverview.component.tsx
+++ src/components/blocks/NationalDictionariesOverview/NationalDictionariesOverview.component.tsx
@@ -17,24 +17,33 @@ export const NationalDictionaryOverviewComponent = ({
   pages,
   moreInfoButtonText,
 }: InterfaceNationalDictionaryOverviewComponentPropTypes): React.JSX.Element => {
-  const allItems = pages.map((item) => (
-    <GenericTeaser
-      className={styles.item}
-      key={item.id}
-      title={rteToHtml(item.hero.title)}
-      texts={[rteToHtml(item.overviewPageProps.teaserText)]}
-      links={[
-        {
+  const allItems = pages.map((item) => {
 
-          // TODO: generate proper url
-          href: `${item.slug}/${item.id}`,
-          text: rteToHtml(moreInfoButtonText),
-          type: 'internal',
-        },
-      ]}
-      type='generic'
-    />
-  ));
+    const image = typeof item.overviewPageProps.image === 'string'
+      ? undefined
+      : item.overviewPageProps.image;
+
+    return (
+      <GenericTeaser
+        className={styles.item}
+        image={image}
+        key={item.id}
+        title={rteToHtml(item.hero.title)}
+        texts={[rteToHtml(item.overviewPageProps.teaserText)]}
+        links={[
+          {
+
+            // TODO: generate proper url
+            href: `${item.slug}/${item.id}`,
+            text: rteToHtml(moreInfoButtonText),
+            type: 'internal',
+          },
+        ]}
+        type='institute'
+      />
+    );
+
+  });
 
   return (
     <GenericOverview
