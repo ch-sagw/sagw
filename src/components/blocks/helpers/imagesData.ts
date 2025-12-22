a0b5c4f19a3c90c48d717d0adf255eafaecf8ab2
Marius Bleuer <m.bleuer@lycos.com>
2025-12-22 09:07:33 +0100

    feat: adding new imageData helper for block stories

---
+++ src/components/blocks/helpers/imagesData.ts
@@ -0,0 +1,81 @@
+import { Image } from '@/payload-types';
+
+export const PersonImage:Image = {
+  alt: '',
+  createdAt: '2025-11-19T09:54:49.430Z',
+  filename: 'csm_Haller_Lea_web_cbe78041a2.jpg',
+  focalX: 50,
+  focalY: 50,
+  height: 650,
+  id: '6942d68d78d7d59f02b53782',
+  updatedAt: '2025-11-19T09:54:49.431Z',
+  // The url value coming from Payload will not contain the hostname.
+  // It is added here for Storybook only.
+  url: 'https://sagw-nu-localhost.gumlet.io/csm_Haller_Lea_web_cbe78041a2.jpg',
+  width: 650,
+};
+
+export const InstitutesTeaserImagePNG:Image = {
+  alt: '',
+  createdAt: '2025-12-21T16:19:16.109Z',
+  filename: 'idrg-institute-teaser-3x.png',
+  focalX: 50,
+  focalY: 50,
+  height: 972,
+  id: '69481e04ef58fcfbceb534e1',
+  mimeType: 'image/svg+xml',
+  updatedAt: '2025-12-21T16:19:16.109Z',
+  url: 'https://sagw-nu-localhost.gumlet.io/idrg-institute-teaser-3x.png',
+  width: 1728,
+};
+
+export const InstitutesTeaserImageSVG:Image = {
+  alt: '',
+  createdAt: '2025-12-21T16:19:26.979Z',
+  filename: 'idrg-institute-teaser.svg',
+  height: 324,
+  id: '69481e0eef58fcfbceb53555',
+  mimeType: 'image/svg+xml',
+  updatedAt: '2025-12-21T16:19:26.979Z',
+  url: 'https://sagw-nu-localhost.gumlet.io/idrg-institute-teaser.svg',
+  width: 576,
+};
+
+export const MagazineTeaserImage:Image = {
+  alt: '',
+  createdAt: '2025-12-17T19:01:20.572Z',
+  filename: 'One_Way_Detailansicht.jpg',
+  focalX: 85,
+  focalY: 50,
+  height: 720,
+  id: '6942fe0078d7d59f02b546e4',
+  updatedAt: '2025-12-18T09:27:43.713Z',
+  url: 'https://sagw-nu-localhost.gumlet.io/One_Way_Detailansicht.jpg',
+  width: 1280,
+};
+
+export const NetworkTeaserImagePNG:Image = {
+  alt: '',
+  createdAt: '2025-12-21T16:34:35.507Z',
+  filename: 'sslas-network-teaser-3x.png',
+  focalX: 50,
+  focalY: 50,
+  height: 300,
+  id: '69427cdc56df0229f95b698e',
+  mimeType: 'image/png',
+  updatedAt: '2025-12-21T16:34:35.507Z',
+  url: 'https://sagw-nu-localhost.gumlet.io/sslas-network-teaser-3x.png',
+  width: 1728,
+};
+
+export const NetworkTeaserImageSVG:Image = {
+  alt: '',
+  createdAt: '2025-12-21T16:31:28.095Z',
+  filename: 'sslas-network-teaser.svg',
+  height: 100,
+  id: '2025-12-21T16:31:28.095Z',
+  mimeType: 'image/svg+xml',
+  updatedAt: '2025-12-18T20:09:00.790Z',
+  url: 'https://sagw-nu-localhost.gumlet.io/sslas-network-teaser.svg',
+  width: 576,
+};
