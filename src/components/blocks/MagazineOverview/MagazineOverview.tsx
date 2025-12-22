c071928d67ca940395224030fb27c2daaf0223d8
Marius Bleuer <m.bleuer@lycos.com>
2025-12-22 09:26:36 +0100

    feat: adding image handling to MagazineOverview component

--- src/components/blocks/MagazineOverview/MagazineOverview.tsx
+++ src/components/blocks/MagazineOverview/MagazineOverview.tsx
@@ -8,15 +8,10 @@ import { fetchDetailPages } from '@/data/fetch';
 import { MagazineOverviewComponent } from '@/components/blocks/MagazineOverview/MagazineOverview.component';
 import { getFirstImageIdOfMagazinePage } from '@/components/helpers/magazineImage';
 import { getLocale } from 'next-intl/server';
-import configPromise from '@/payload.config';
-import {
-  getPayload,
-  TypedLocale,
-} from 'payload';
+import { TypedLocale } from 'payload';
 
 export type InterfaceMagazineOverviewPropTypes = {
   tenant: string;
-  image: Image;
 } & InterfaceMagazineOverviewBlock;
 
 export type InterfaceMagazineDetailPageWithImage = {
@@ -24,11 +19,8 @@ export type InterfaceMagazineDetailPageWithImage = {
 } & MagazineDetailPage;
 
 export const MagazineOverview = async (props: InterfaceMagazineOverviewPropTypes): Promise<React.JSX.Element> => {
-  const payload = await getPayload({
-    config: configPromise,
-  });
-
   const locale = (await getLocale()) as TypedLocale;
+
   const {
     tenant,
     ...restProps
@@ -36,41 +28,19 @@ export const MagazineOverview = async (props: InterfaceMagazineOverviewPropTypes
 
   const pages = await fetchDetailPages({
     collection: 'magazineDetailPage',
+    depth: 1,
     language: locale,
     limit: 0,
     sort: 'createdAt',
     tenant,
   }) as InterfaceMagazineDetailPageWithImage[];
 
-  const images = await payload.find({
-    collection: 'images',
-    depth: 0,
-    limit: 0,
-    pagination: false,
-    sort: 'createdAt',
-    where: {
-      tenant: {
-        equals: tenant,
-      },
-    },
-  });
-
   // Find matching image data for magazine teaser items
-  const teaserImageIds = pages.map((page: MagazineDetailPage) => getFirstImageIdOfMagazinePage(page));
-
-  const imagesData = teaserImageIds.map((id) => {
-    if (!id) {
-      return '';
-    }
-
-    const match = images.docs.find((img) => img.id === id);
-
-    return match || '';
-  });
+  const teaserImages = pages.map((page: MagazineDetailPage) => getFirstImageIdOfMagazinePage(page));
 
   // Attaching image data to magazine teaser items
   for (let i = 0, p = pages.length; i < p; i++) {
-    pages[i].image = imagesData[i];
+    pages[i].image = teaserImages[i];
   }
 
   return (
