122f2a29a0e0cfe9e376cb59a88e646e4fffa868
Yves Tscherry <info@feerglas.com>
2026-01-10 12:35:16 +0100

    test: fix links tests

--- src/field-templates/links.be.spec.ts
+++ src/field-templates/links.be.spec.ts
@@ -8,176 +8,188 @@ import {
 import { getTenant } from '@/test-helpers/tenant-generator';
 import { simpleRteConfig } from '@/utilities/simpleRteConfig';
 import { EventDetailPage } from '@/payload-types';
+import {
+  deleteOtherCollections, deleteSetsPages,
+} from '@/seed/test-data/deleteData';
 
-test('throws error on invalid external url', async () => {
-  const tenant = await getTenant();
-  const payload = await getPayload({
-    config: configPromise,
+test.describe('links', () => {
+  test.beforeEach(async () => {
+
+    // delete data
+    await deleteSetsPages();
+    await deleteOtherCollections();
   });
 
-  let result;
-
-  try {
-    /* eslint-disable @typescript-eslint/naming-convention */
-    const createEventResult = await payload.create({
-      collection: 'eventDetailPage',
-      data: {
-        _status: 'published',
-        eventDetails: {
-          date: '2025-08-31T12:00:00.000Z',
-          title: simpleRteConfig(`${(new Date())
-            .toString()} - 1`),
-        },
-        link: {
-          externalLink: '.foo.bar',
-        },
-        showDetailPage: 'false',
-        slug: `${(new Date())
-          .toString()} - 1`,
-        tenant,
-      },
-      draft: false,
+  test('throws error on invalid external url', async () => {
+    const tenant = await getTenant();
+    const payload = await getPayload({
+      config: configPromise,
     });
 
-    result = createEventResult;
+    let result;
+
+    try {
+    /* eslint-disable @typescript-eslint/naming-convention */
+      const createEventResult = await payload.create({
+        collection: 'eventDetailPage',
+        data: {
+          _status: 'published',
+          eventDetails: {
+            date: '2025-08-31T12:00:00.000Z',
+            title: simpleRteConfig(`${(new Date())
+              .toString()} - 1`),
+          },
+          link: {
+            externalLink: '.foo.bar',
+          },
+          showDetailPage: 'false',
+          slug: `${(new Date())
+            .toString()} - 1`,
+          tenant,
+        },
+        draft: false,
+      });
+
+      result = createEventResult;
     /* eslint-enable @typescript-eslint/naming-convention */
-  } catch (e) {
-    result = JSON.stringify(e);
-  }
+    } catch (e) {
+      result = JSON.stringify(e);
+    }
 
-  /* eslint-disable no-useless-escape */
-  await expect(result)
-    .toStrictEqual('{\"data\":{\"collection\":\"eventDetailPage\",\"errors\":[{\"label\":\"Content > Link > External Link\",\"message\":\"The URL has an invalid format. The URL must have a format like https://www.google.com, https://google.com, or www.google.com.\",\"path\":\"link.externalLink\"}]},\"isOperational\":true,\"isPublic\":true,\"status\":400,\"name\":\"ValidationError\"}');
-  /* eslint-enable no-useless-escape */
+    /* eslint-disable no-useless-escape */
+    await expect(result)
+      .toStrictEqual('{\"data\":{\"collection\":\"eventDetailPage\",\"errors\":[{\"label\":\"Content > Link > External Link\",\"message\":\"The URL has an invalid format. The URL must have a format like https://www.google.com, https://google.com, or www.google.com.\",\"path\":\"link.externalLink\"}]},\"isOperational\":true,\"isPublic\":true,\"status\":400,\"name\":\"ValidationError\"}');
+    /* eslint-enable no-useless-escape */
 
-});
-
-test('does not throw an error on valid external url', async () => {
-  const tenant = await getTenant();
-  const payload = await getPayload({
-    config: configPromise,
   });
 
-  let result: EventDetailPage | undefined;
-
-  try {
-    /* eslint-disable @typescript-eslint/naming-convention */
-    const createEventResult = await payload.create({
-      collection: 'eventDetailPage',
-      data: {
-        _status: 'published',
-        eventDetails: {
-          date: '2025-08-31T12:00:00.000Z',
-          title: simpleRteConfig(`${(new Date())
-            .toString()} - 2`),
-        },
-        link: {
-          externalLink: 'https://www.foo.bar',
-        },
-        showDetailPage: 'false',
-        slug: `${(new Date())
-          .toString()} - 2`,
-        tenant,
-      },
-      draft: false,
+  test('does not throw an error on valid external url', async () => {
+    const tenant = await getTenant();
+    const payload = await getPayload({
+      config: configPromise,
     });
 
-    result = createEventResult;
+    let result: EventDetailPage | undefined;
+
+    try {
+    /* eslint-disable @typescript-eslint/naming-convention */
+      const createEventResult = await payload.create({
+        collection: 'eventDetailPage',
+        data: {
+          _status: 'published',
+          eventDetails: {
+            date: '2025-08-31T12:00:00.000Z',
+            title: simpleRteConfig(`${(new Date())
+              .toString()} - 2`),
+          },
+          link: {
+            externalLink: 'https://www.foo.bar',
+          },
+          showDetailPage: 'false',
+          slug: `${(new Date())
+            .toString()} - 2`,
+          tenant,
+        },
+        draft: false,
+      });
+
+      result = createEventResult;
     /* eslint-enable @typescript-eslint/naming-convention */
-  } catch {
-    result = undefined;
-  }
+    } catch {
+      result = undefined;
+    }
 
-  const linkResult = result?.link?.externalLink;
+    const linkResult = result?.link?.externalLink;
 
-  await expect(linkResult)
-    .toStrictEqual('https://www.foo.bar');
+    await expect(linkResult)
+      .toStrictEqual('https://www.foo.bar');
 
-});
-
-test('does not throw an error on valid external url with path segment', async () => {
-  const tenant = await getTenant();
-  const payload = await getPayload({
-    config: configPromise,
   });
 
-  let result: EventDetailPage | undefined;
-
-  try {
-    /* eslint-disable @typescript-eslint/naming-convention */
-    const createEventResult = await payload.create({
-      collection: 'eventDetailPage',
-      data: {
-        _status: 'published',
-        eventDetails: {
-          date: '2025-07-31T12:00:00.000Z',
-          title: simpleRteConfig(`${(new Date())
-            .toString()} - 3`),
-        },
-        link: {
-          externalLink: 'https://www.foo.bar/baz',
-        },
-        showDetailPage: 'false',
-        slug: `${(new Date())
-          .toString()} - 3`,
-        tenant,
-      },
-      draft: false,
+  test('does not throw an error on valid external url with path segment', async () => {
+    const tenant = await getTenant();
+    const payload = await getPayload({
+      config: configPromise,
     });
 
-    result = createEventResult;
+    let result: EventDetailPage | undefined;
+
+    try {
+    /* eslint-disable @typescript-eslint/naming-convention */
+      const createEventResult = await payload.create({
+        collection: 'eventDetailPage',
+        data: {
+          _status: 'published',
+          eventDetails: {
+            date: '2025-07-31T12:00:00.000Z',
+            title: simpleRteConfig(`${(new Date())
+              .toString()} - 3`),
+          },
+          link: {
+            externalLink: 'https://www.foo.bar/baz',
+          },
+          showDetailPage: 'false',
+          slug: `${(new Date())
+            .toString()} - 3`,
+          tenant,
+        },
+        draft: false,
+      });
+
+      result = createEventResult;
     /* eslint-enable @typescript-eslint/naming-convention */
-  } catch {
-    result = undefined;
-  }
+    } catch {
+      result = undefined;
+    }
 
-  const linkResult = result?.link?.externalLink;
+    const linkResult = result?.link?.externalLink;
 
-  await expect(linkResult)
-    .toStrictEqual('https://www.foo.bar/baz');
+    await expect(linkResult)
+      .toStrictEqual('https://www.foo.bar/baz');
 
-});
-
-test('does not throw an error on valid external without www', async () => {
-  const tenant = await getTenant();
-  const payload = await getPayload({
-    config: configPromise,
   });
 
-  let result: EventDetailPage | undefined;
-
-  try {
-    /* eslint-disable @typescript-eslint/naming-convention */
-    const createEventResult = await payload.create({
-      collection: 'eventDetailPage',
-      data: {
-        _status: 'published',
-        eventDetails: {
-          date: '2025-07-31T12:00:00.000Z',
-          title: simpleRteConfig(`${(new Date())
-            .toString()} - 4`),
-        },
-        link: {
-          externalLink: 'https://foo.bar',
-        },
-        showDetailPage: 'false',
-        slug: `${(new Date())
-          .toString()} - 4`,
-        tenant,
-      },
-      draft: false,
+  test('does not throw an error on valid external without www', async () => {
+    const tenant = await getTenant();
+    const payload = await getPayload({
+      config: configPromise,
     });
 
-    result = createEventResult;
+    let result: EventDetailPage | undefined;
+
+    try {
+    /* eslint-disable @typescript-eslint/naming-convention */
+      const createEventResult = await payload.create({
+        collection: 'eventDetailPage',
+        data: {
+          _status: 'published',
+          eventDetails: {
+            date: '2025-07-31T12:00:00.000Z',
+            title: simpleRteConfig(`${(new Date())
+              .toString()} - 4`),
+          },
+          link: {
+            externalLink: 'https://foo.bar',
+          },
+          showDetailPage: 'false',
+          slug: `${(new Date())
+            .toString()} - 4`,
+          tenant,
+        },
+        draft: false,
+      });
+
+      result = createEventResult;
 
     /* eslint-enable @typescript-eslint/naming-convention */
-  } catch {
-    result = undefined;
-  }
+    } catch {
+      result = undefined;
+    }
 
-  const linkResult = result?.link?.externalLink;
+    const linkResult = result?.link?.externalLink;
 
-  await expect(linkResult)
-    .toStrictEqual('https://foo.bar');
+    await expect(linkResult)
+      .toStrictEqual('https://foo.bar');
 
+  });
 });
