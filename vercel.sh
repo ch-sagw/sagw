b5734b5d62acb6d2966c2044e9a4fbcc33153d2b
Yves Tscherry <info@feerglas.com>
2026-01-21 08:50:12 +0100

    fix: export env

--- vercel.sh
+++ vercel.sh
@@ -4,7 +4,10 @@ set -euo pipefail
 TARGET="${BUILD_TARGET:-payload}"  # default to payload
 
 case "$TARGET" in
-  payload)   npm run build:prod ;;
+  payload)
+    export ENV=prod
+    npm run build:prod
+    ;;
   storybook) npm run build-storybook ;;
   *)
     echo "Unknown BUILD_TARGET: $TARGET" >&2
