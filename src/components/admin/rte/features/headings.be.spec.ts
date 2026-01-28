aea424761b3f28dc1585374332c42a1fd07b2d8b
Yves Tscherry <info@feerglas.com>
2026-01-28 10:00:56 +0100

    Feat/rte heading structure (#729)
    * feat: transform rte heading levels up
    
    * feat: add id's to rte headings

---
+++ src/components/admin/rte/features/headings.be.spec.ts
@@ -0,0 +1,581 @@
+import {
+  expect,
+  test,
+} from '@playwright/test';
+import {
+  generateDetailPage,
+  getHomeId,
+} from '@/test-helpers/collections-generator';
+import { getTenantId } from '@/test-helpers/tenant-generator';
+import { getPayloadCached } from '@/utilities/getPayloadCached';
+import { deleteSetsPages } from '@/seed/test-data/deleteData';
+import { beforeEachAcceptCookies } from '@/test-helpers/cookie-consent';
+
+/*
+  rte content with h1, h2, h3, h4, h5 and some text in between, to test
+  to check correct heading level rendering
+*/
+const rteContent = {
+  root: {
+
+    children: [
+
+      {
+
+        children: [
+
+          {
+            detail: 0,
+            mode: 'normal',
+            style: '',
+            text: 'h1',
+            type: 'text',
+            version: 1,
+          },
+        ],
+        direction: null,
+        indent: 0,
+        tag: 'h1',
+        type: 'heading',
+        version: 1,
+      },
+
+      {
+
+        children: [
+
+          {
+            detail: 0,
+            mode: 'normal',
+            style: '',
+            text: 'text',
+            type: 'text',
+            version: 1,
+          },
+        ],
+        direction: null,
+        indent: 0,
+        textFormat: 0,
+        textStyle: '',
+        type: 'paragraph',
+        version: 1,
+      },
+
+      {
+
+        children: [
+
+          {
+            detail: 0,
+            mode: 'normal',
+            style: '',
+            text: 'h2',
+            type: 'text',
+            version: 1,
+          },
+        ],
+        direction: null,
+        indent: 0,
+        tag: 'h2',
+        type: 'heading',
+        version: 1,
+      },
+
+      {
+
+        children: [
+
+          {
+            detail: 0,
+            mode: 'normal',
+            style: '',
+            text: 'text',
+            type: 'text',
+            version: 1,
+          },
+        ],
+        direction: null,
+        indent: 0,
+        textFormat: 0,
+        textStyle: '',
+        type: 'paragraph',
+        version: 1,
+      },
+
+      {
+
+        children: [
+
+          {
+            detail: 0,
+            mode: 'normal',
+            style: '',
+            text: 'h3',
+            type: 'text',
+            version: 1,
+          },
+        ],
+        direction: null,
+        indent: 0,
+        tag: 'h3',
+        type: 'heading',
+        version: 1,
+      },
+
+      {
+
+        children: [
+
+          {
+            detail: 0,
+            mode: 'normal',
+            style: '',
+            text: 'text',
+            type: 'text',
+            version: 1,
+          },
+        ],
+        direction: null,
+        indent: 0,
+        textFormat: 0,
+        textStyle: '',
+        type: 'paragraph',
+        version: 1,
+      },
+
+      {
+
+        children: [
+
+          {
+            detail: 0,
+            mode: 'normal',
+            style: '',
+            text: 'h4',
+            type: 'text',
+            version: 1,
+          },
+        ],
+        direction: null,
+        indent: 0,
+        tag: 'h4',
+        type: 'heading',
+        version: 1,
+      },
+
+      {
+
+        children: [
+
+          {
+            detail: 0,
+            mode: 'normal',
+            style: '',
+            text: 'text',
+            type: 'text',
+            version: 1,
+          },
+        ],
+        direction: null,
+        indent: 0,
+        textFormat: 0,
+        textStyle: '',
+        type: 'paragraph',
+        version: 1,
+      },
+
+      {
+
+        children: [
+
+          {
+            detail: 0,
+            mode: 'normal',
+            style: '',
+            text: 'h5',
+            type: 'text',
+            version: 1,
+          },
+        ],
+        direction: null,
+        indent: 0,
+        tag: 'h5',
+        type: 'heading',
+        version: 1,
+      },
+
+      {
+
+        children: [
+
+          {
+            detail: 0,
+            mode: 'normal',
+            style: '',
+            text: 'text',
+            type: 'text',
+            version: 1,
+          },
+        ],
+        direction: null,
+        indent: 0,
+        textFormat: 0,
+        textStyle: '',
+        type: 'paragraph',
+        version: 1,
+      },
+    ],
+    direction: null,
+    indent: 0,
+    type: 'root',
+    version: 1,
+  },
+};
+
+/*
+  rte content with h1, h2, h3, h4, h5 and some text in between. All headings
+  have the same text, to test proper id generation
+*/
+const rteContent2 = {
+  root: {
+
+    children: [
+
+      {
+
+        children: [
+
+          {
+            detail: 0,
+            mode: 'normal',
+            style: '',
+            text: 'h1',
+            type: 'text',
+            version: 1,
+          },
+        ],
+        direction: null,
+        indent: 0,
+        tag: 'h1',
+        type: 'heading',
+        version: 1,
+      },
+
+      {
+
+        children: [
+
+          {
+            detail: 0,
+            mode: 'normal',
+            style: '',
+            text: 'text',
+            type: 'text',
+            version: 1,
+          },
+        ],
+        direction: null,
+        indent: 0,
+        textFormat: 0,
+        textStyle: '',
+        type: 'paragraph',
+        version: 1,
+      },
+
+      {
+
+        children: [
+
+          {
+            detail: 0,
+            mode: 'normal',
+            style: '',
+            text: 'h1',
+            type: 'text',
+            version: 1,
+          },
+        ],
+        direction: null,
+        indent: 0,
+        tag: 'h2',
+        type: 'heading',
+        version: 1,
+      },
+
+      {
+
+        children: [
+
+          {
+            detail: 0,
+            mode: 'normal',
+            style: '',
+            text: 'text',
+            type: 'text',
+            version: 1,
+          },
+        ],
+        direction: null,
+        indent: 0,
+        textFormat: 0,
+        textStyle: '',
+        type: 'paragraph',
+        version: 1,
+      },
+
+      {
+
+        children: [
+
+          {
+            detail: 0,
+            mode: 'normal',
+            style: '',
+            text: 'h1',
+            type: 'text',
+            version: 1,
+          },
+        ],
+        direction: null,
+        indent: 0,
+        tag: 'h3',
+        type: 'heading',
+        version: 1,
+      },
+
+      {
+
+        children: [
+
+          {
+            detail: 0,
+            mode: 'normal',
+            style: '',
+            text: 'text',
+            type: 'text',
+            version: 1,
+          },
+        ],
+        direction: null,
+        indent: 0,
+        textFormat: 0,
+        textStyle: '',
+        type: 'paragraph',
+        version: 1,
+      },
+
+      {
+
+        children: [
+
+          {
+            detail: 0,
+            mode: 'normal',
+            style: '',
+            text: 'h1',
+            type: 'text',
+            version: 1,
+          },
+        ],
+        direction: null,
+        indent: 0,
+        tag: 'h4',
+        type: 'heading',
+        version: 1,
+      },
+
+      {
+
+        children: [
+
+          {
+            detail: 0,
+            mode: 'normal',
+            style: '',
+            text: 'text',
+            type: 'text',
+            version: 1,
+          },
+        ],
+        direction: null,
+        indent: 0,
+        textFormat: 0,
+        textStyle: '',
+        type: 'paragraph',
+        version: 1,
+      },
+
+      {
+
+        children: [
+
+          {
+            detail: 0,
+            mode: 'normal',
+            style: '',
+            text: 'h1',
+            type: 'text',
+            version: 1,
+          },
+        ],
+        direction: null,
+        indent: 0,
+        tag: 'h5',
+        type: 'heading',
+        version: 1,
+      },
+
+      {
+
+        children: [
+
+          {
+            detail: 0,
+            mode: 'normal',
+            style: '',
+            text: 'text',
+            type: 'text',
+            version: 1,
+          },
+        ],
+        direction: null,
+        indent: 0,
+        textFormat: 0,
+        textStyle: '',
+        type: 'paragraph',
+        version: 1,
+      },
+    ],
+    direction: null,
+    indent: 0,
+    type: 'root',
+    version: 1,
+  },
+};
+
+test.describe('heading levels and ids', () => {
+  beforeEachAcceptCookies();
+  test('renders proper heading levels in rte', async ({
+    page,
+  }) => {
+    await deleteSetsPages();
+
+    const payload = await getPayloadCached();
+    const time = (new Date())
+      .getTime();
+
+    const tenant = await getTenantId({
+      isSagw: true,
+      time,
+    });
+
+    const home = await getHomeId({
+      isSagw: true,
+      tenant,
+    });
+
+    const detailPage = await generateDetailPage({
+      parentPage: {
+        documentId: home,
+        slug: 'homePage',
+      },
+      title: `detail ${time}`,
+    });
+
+    await payload.update({
+      collection: 'detailPage',
+      data: {
+        content: [
+          {
+            blockType: 'textBlock',
+            text: rteContent,
+          },
+        ],
+      },
+      id: detailPage.id,
+    });
+
+    await page.goto(`http://localhost:3000/de/detail-${time}`);
+    await page.waitForLoadState('networkidle');
+
+    const h2 = (await page.locator('h2')).nth(0);
+    const h3 = (await page.locator('h3')).nth(0);
+    const h4 = (await page.locator('h4')).nth(0);
+    const h5 = (await page.locator('h5')).nth(0);
+    const h6 = (await page.locator('h6')).nth(0);
+
+    await expect(h2)
+      .toHaveText('h1');
+    await expect(h3)
+      .toHaveText('h2');
+    await expect(h4)
+      .toHaveText('h3');
+    await expect(h5)
+      .toHaveText('h4');
+    await expect(h6)
+      .toHaveText('h5');
+
+  });
+
+  test('renders proper heading ids', async ({
+    page,
+  }) => {
+    await deleteSetsPages();
+
+    const payload = await getPayloadCached();
+    const time = (new Date())
+      .getTime();
+
+    const tenant = await getTenantId({
+      isSagw: true,
+      time,
+    });
+
+    const home = await getHomeId({
+      isSagw: true,
+      tenant,
+    });
+
+    const detailPage = await generateDetailPage({
+      parentPage: {
+        documentId: home,
+        slug: 'homePage',
+      },
+      title: `detail ${time}`,
+    });
+
+    await payload.update({
+      collection: 'detailPage',
+      data: {
+        content: [
+          {
+            blockType: 'textBlock',
+            text: rteContent2,
+          },
+        ],
+      },
+      id: detailPage.id,
+    });
+
+    await page.goto(`http://localhost:3000/de/detail-${time}`);
+    await page.waitForLoadState('networkidle');
+
+    const h2 = (await page.locator('h2')).nth(0);
+    const h3 = (await page.locator('h3')).nth(0);
+    const h4 = (await page.locator('h4')).nth(0);
+    const h5 = (await page.locator('h5')).nth(0);
+    const h6 = (await page.locator('h6')).nth(0);
+
+    await expect(h2)
+      .toHaveId('h1');
+    await expect(h3)
+      .toHaveId('h1-1');
+    await expect(h4)
+      .toHaveId('h1-2');
+    await expect(h5)
+      .toHaveId('h1-3');
+    await expect(h6)
+      .toHaveId('h1-4');
+
+  });
+});
