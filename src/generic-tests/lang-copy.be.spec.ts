58eafa5f31e675ff709213f8c1c9ed12c0938345
Yves Tscherry <info@feerglas.com>
2026-03-25 09:54:06 +0100

    test: add test for proper language copy

---
+++ src/generic-tests/lang-copy.be.spec.ts
@@ -0,0 +1,121 @@
+import {
+  expect,
+  test,
+} from '@playwright/test';
+import { beforeEachPayloadLogin } from '@/test-helpers/payload-login';
+import {
+  deleteOtherCollections, deleteSetsPages,
+} from '@/seed/test-data/deleteData';
+import { getTenantId } from '@/test-helpers/tenant-generator';
+import {
+  generateDetailPage, getHomeId,
+} from '@/test-helpers/collections-generator';
+
+test.describe('Language copy', () => {
+  beforeEachPayloadLogin();
+
+  test('does not error and copies values', async ({
+    page,
+  }) => {
+
+    await deleteSetsPages();
+    await deleteOtherCollections();
+
+    // create a detail page
+    const time = (new Date())
+      .getTime();
+
+    const tenant = await getTenantId({
+      isSagw: true,
+      time,
+    });
+
+    const home = await getHomeId({
+      isSagw: true,
+      tenant,
+    });
+
+    await generateDetailPage({
+      locale: 'de',
+      parentPage: {
+        documentId: home,
+        slug: 'homePage',
+      },
+      tenant,
+      title: `detail 1 ${time}`,
+    });
+
+    // ensure correct language
+    const langSelect = await page.locator('.localizer.app-header__localizer button');
+
+    await langSelect.click();
+
+    const germanLang = await page.locator('.popup-button-list__button')
+      .first();
+
+    await germanLang.click({
+      force: true,
+    });
+
+    // go to detail page
+
+    await page.goto('http://localhost:3000/admin/collections/detailPage?depth=1&limit=10');
+    await page.waitForLoadState('networkidle');
+
+    const detailPageEntry = await page.getByText(`detail 1 ${time}`);
+
+    await detailPageEntry.click();
+
+    await page.waitForLoadState('networkidle');
+
+    // make language copy
+    const threeDotsButton = await page.locator('.popup.doc-controls__popup button.popup-button');
+
+    await threeDotsButton.click({
+      force: true,
+    });
+
+    const duplicateButton = await page.locator('#copy-locale-data__button');
+
+    await duplicateButton.click();
+
+    // language copy overlay
+    const copyToSelectField = await page.locator('#field-toLocale .field-type__wrap');
+
+    await copyToSelectField.click();
+
+    const langOptions = await page.locator('.rs__option')
+      .first();
+
+    await langOptions.click();
+
+    // copy button
+    const copyButton = await page.locator('.copy-locale-data__sub-header button');
+
+    await copyButton.click();
+
+    // verify we're on the correct language
+    const langButtonNew = await langSelect.locator('.localizer-button__current-label');
+
+    await expect(langButtonNew)
+      .toHaveText('Français');
+
+    // verify hero content
+    const heroField = await page.locator('#field-hero .rich-text-lexical:first-of-type p.LexicalEditorTheme__paragraph');
+
+    await expect(heroField)
+      .toHaveText(`detail 1 ${time}`);
+
+    // verify meta content
+    const metaTab = await page.locator('.tabs-field__tab-button')
+      .nth(1);
+
+    await metaTab.click();
+
+    const metaTitle = await page.locator('#field-meta__seo__title');
+
+    await expect(metaTitle)
+      .toHaveValue('seo title');
+  });
+
+});
