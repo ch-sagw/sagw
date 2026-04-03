0e79351622100e2436864259df7b937263afc67f
Yves Tscherry <info@feerglas.com>
2026-04-03 18:19:19 +0200

    fix: try to fix url host

--- src/utilities/getUrl.ts
+++ src/utilities/getUrl.ts
@@ -1,15 +1,16 @@
 export const getServerSideURL = (): string => {
-  // We set NEXT_PUBLIC_SERVER_URL explicitly on prod and test
-  // environments. try to use VERCEL_PROJECT_PRODUCTION_URL for preview
-  // deployments, which should be injected by vercel automatically.
-  if (process.env.VERCEL_PROJECT_PRODUCTION_URL) {
-    return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
-  }
 
+  // Prod and Test Env
   if (process.env.NEXT_PUBLIC_SERVER_URL) {
     return process.env.NEXT_PUBLIC_SERVER_URL;
   }
 
+  // Preview Env
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`;
+  }
+
+  // Local dev env
   return 'http://localhost:3000';
 };
 
