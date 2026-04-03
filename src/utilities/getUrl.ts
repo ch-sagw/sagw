e01e1b93ef3f475f943771dc954e83db7939b3b3
Yves Tscherry <info@feerglas.com>
2026-04-03 18:12:02 +0200

    fix: make sure to return vercel injected host if available

--- src/utilities/getUrl.ts
+++ src/utilities/getUrl.ts
@@ -2,17 +2,15 @@ export const getServerSideURL = (): string => {
   // We set NEXT_PUBLIC_SERVER_URL explicitly on prod and test
   // environments. try to use VERCEL_PROJECT_PRODUCTION_URL for preview
   // deployments, which should be injected by vercel automatically.
-  let url = process.env.NEXT_PUBLIC_SERVER_URL;
-
-  if (!url && process.env.VERCEL_PROJECT_PRODUCTION_URL) {
+  if (process.env.VERCEL_PROJECT_PRODUCTION_URL) {
     return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
   }
 
-  if (!url) {
-    url = 'http://localhost:3000';
+  if (process.env.NEXT_PUBLIC_SERVER_URL) {
+    return process.env.NEXT_PUBLIC_SERVER_URL;
   }
 
-  return url;
+  return 'http://localhost:3000';
 };
 
 export const absoluteUrlFromPathname = (pathname: string): string => {
