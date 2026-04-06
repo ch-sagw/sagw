39cb25c5fa2941786009fdcc68a229aa93dd474f
Yves Tscherry <info@feerglas.com>
2026-04-06 10:36:08 +0200

    Fix/proper render rte urls (#970)
    fix: fix issue for svg rendering in html converter
    test: add story with rte content
    test: add tests for fe rendering of external urls and email addresses

--- src/components/base/Notification/Notification.stories.ts
+++ src/components/base/Notification/Notification.stories.ts
@@ -92,3 +92,17 @@ export const WithLink: StrictStory = {
     type: 'warn',
   },
 };
+
+export const WithRteContent: StrictStory = {
+  args: {
+    ...defaultProps,
+    actionText: undefined,
+    hideBorder: true,
+    hideIcon: true,
+    linkHref: undefined,
+    linkText: undefined,
+    onAction: undefined,
+    text: 'Some link text <b>bold</b> <a href="#">link<svg role="presentation" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12.8184 9.18177L21 1" strokewidth="1.5"></path><path d="M20.9994 7.54542L20.9984 1.00102L14.4541 1" strokelinecap="square" strokewidth="1.5"></path><path d="M17.727 10.8181V18.1817C17.727 18.3987 17.727 18.9999 17.727 18.9999C17.727 18.9999 17.1258 18.9999 16.9089 18.9999H3.81817C3.60118 18.9999 3 18.9999 3 18.9999C3 18.9999 3 18.3987 3 18.1817V5.09088C3 4.87389 3 4.27271 3 4.27271C3 4.27271 3.60118 4.27271 3.81817 4.27271H11.1817" strokewidth="1.5"></path></svg></a>',
+    type: 'success',
+  },
+};
