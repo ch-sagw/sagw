6d9803447eef3adbda70b7514928d14787117a9c
Yves Tscherry <info@feerglas.com>
2026-04-08 16:32:13 +0200

    fix: make sure redirects and tenant-aware error pages work

--- src/app/(frontend)/[locale]/[...slug]/page.tsx
+++ src/app/(frontend)/[locale]/[...slug]/page.tsx
@@ -6,7 +6,8 @@ import { InterfaceOtherPagesProps } from '@/app/(frontend)/fetchers/otherPages';
 import { Metadata } from 'next';
 import { renderMeta } from '@/app/(frontend)/renderers/RenderMeta';
 import { getPageData } from '../../fetchers/pageData';
-import { RenderNotFoundPage } from '../../renderers/RenderNotFoundPage';
+import { runRedirectIfMatch } from '@/components/helpers/redirects';
+import { RenderNotFoundPage } from '@/app/(frontend)/renderers/RenderNotFoundPage';
 
 export { generateStaticParams };
 
@@ -38,6 +39,11 @@ export default async function Page({
   });
 
   if (!pageData) {
+    await runRedirectIfMatch({
+      locale,
+      url: slug.join('/'),
+    });
+
     return (
       <RenderNotFoundPage
         locale={locale}
