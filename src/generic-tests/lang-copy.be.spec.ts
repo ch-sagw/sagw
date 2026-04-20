7553572226395da49720d17e2817b456bf0c6d27
Yves Tscherry <info@feerglas.com>
2026-04-20 18:13:09 +0200

    test: add tests for the failing collections

--- src/generic-tests/lang-copy.be.spec.ts
+++ src/generic-tests/lang-copy.be.spec.ts
@@ -8,13 +8,13 @@ import {
 } from '@/seed/test-data/deleteData';
 import { getTenantId } from '@/test-helpers/tenant-generator';
 import {
-  generateDetailPage, getHomeId,
+  generateDetailPage, generateInstituteDetailPage, generateNationalDictionaryDetailPage, getHomeId,
 } from '@/test-helpers/collections-generator';
 
 test.describe('Language copy', () => {
   beforeEachPayloadLogin();
 
-  test('does not error and copies values', async ({
+  test('detail page: does not error and copies values', async ({
     page,
   }) => {
 
@@ -59,6 +59,224 @@ test.describe('Language copy', () => {
     // go to detail page
 
     await page.goto('http://localhost:3000/admin/collections/detailPage?depth=1&limit=10');
+    await page.waitForLoadState('networkidle');
+
+    const detailPageEntry = await page.getByText(`detail 1 ${time}`);
+
+    await detailPageEntry.click();
+
+    await page.waitForLoadState('networkidle');
+
+    // make language copy
+    const threeDotsButton = await page.locator('.popup.doc-controls__popup button.popup-button');
+
+    await threeDotsButton.click({
+      force: true,
+    });
+
+    const duplicateButton = await page.locator('#copy-locale-data__button');
+
+    await duplicateButton.click();
+
+    // language copy overlay
+    const copyToSelectField = await page.locator('#field-toLocale .field-type__wrap');
+
+    await copyToSelectField.click();
+
+    const langOptions = await copyToSelectField.getByText('Italiano');
+
+    await langOptions.click();
+
+    // copy button
+    const copyButton = await page.locator('.copy-locale-data__sub-header button');
+
+    await copyButton.click();
+
+    // verify we're on the correct language
+    const langButtonNew = await langSelect.locator('.localizer-button__current-label');
+
+    await expect(langButtonNew)
+      .toHaveText('Italiano');
+
+    // verify hero content
+    const heroField = await page.locator('#field-hero .rich-text-lexical:first-of-type p.LexicalEditorTheme__paragraph');
+
+    await expect(heroField)
+      .toHaveText(`detail 1 ${time}`);
+
+    // verify meta content
+    const metaTab = await page.locator('.tabs-field__tab-button')
+      .nth(1);
+
+    await metaTab.click();
+
+    const metaTitle = await page.locator('#field-meta__seo__title');
+
+    await expect(metaTitle)
+      .toHaveValue('seo title');
+
+    // reset language to german
+    await langSelect.click();
+
+    await await germanLang.click({
+      force: true,
+    });
+  });
+
+  test('institue detail page: does not error and copies values', async ({
+    page,
+  }) => {
+
+    await deleteSetsPages();
+    await deleteOtherCollections();
+
+    // create a detail page
+    const time = (new Date())
+      .getTime();
+
+    const tenant = await getTenantId({
+      isSagw: true,
+      time,
+    });
+
+    const home = await getHomeId({
+      isSagw: true,
+      tenant,
+    });
+
+    await generateInstituteDetailPage({
+      locale: 'de',
+      parentPage: {
+        documentId: home,
+        slug: 'homePage',
+      },
+      tenant,
+      title: `detail 1 ${time}`,
+    });
+
+    // ensure correct language
+    const langSelect = await page.locator('.localizer.app-header__localizer');
+
+    await langSelect.click();
+
+    const germanLang = await page.locator('.localizer__locale-code[data-locale="de"]');
+
+    await germanLang.click({
+      force: true,
+    });
+
+    // go to detail page
+
+    await page.goto('http://localhost:3000/admin/collections/instituteDetailPage?depth=1&limit=10');
+    await page.waitForLoadState('networkidle');
+
+    const detailPageEntry = await page.getByText(`detail 1 ${time}`);
+
+    await detailPageEntry.click();
+
+    await page.waitForLoadState('networkidle');
+
+    // make language copy
+    const threeDotsButton = await page.locator('.popup.doc-controls__popup button.popup-button');
+
+    await threeDotsButton.click({
+      force: true,
+    });
+
+    const duplicateButton = await page.locator('#copy-locale-data__button');
+
+    await duplicateButton.click();
+
+    // language copy overlay
+    const copyToSelectField = await page.locator('#field-toLocale .field-type__wrap');
+
+    await copyToSelectField.click();
+
+    const langOptions = await copyToSelectField.getByText('Italiano');
+
+    await langOptions.click();
+
+    // copy button
+    const copyButton = await page.locator('.copy-locale-data__sub-header button');
+
+    await copyButton.click();
+
+    // verify we're on the correct language
+    const langButtonNew = await langSelect.locator('.localizer-button__current-label');
+
+    await expect(langButtonNew)
+      .toHaveText('Italiano');
+
+    // verify hero content
+    const heroField = await page.locator('#field-hero .rich-text-lexical:first-of-type p.LexicalEditorTheme__paragraph');
+
+    await expect(heroField)
+      .toHaveText(`detail 1 ${time}`);
+
+    // verify meta content
+    const metaTab = await page.locator('.tabs-field__tab-button')
+      .nth(1);
+
+    await metaTab.click();
+
+    const metaTitle = await page.locator('#field-meta__seo__title');
+
+    await expect(metaTitle)
+      .toHaveValue('seo title');
+
+    // reset language to german
+    await langSelect.click();
+
+    await await germanLang.click({
+      force: true,
+    });
+  });
+
+  test('national dictionary detail page: does not error and copies values', async ({
+    page,
+  }) => {
+
+    await deleteSetsPages();
+    await deleteOtherCollections();
+
+    // create a detail page
+    const time = (new Date())
+      .getTime();
+
+    const tenant = await getTenantId({
+      isSagw: true,
+      time,
+    });
+
+    const home = await getHomeId({
+      isSagw: true,
+      tenant,
+    });
+
+    await generateNationalDictionaryDetailPage({
+      locale: 'de',
+      parentPage: {
+        documentId: home,
+        slug: 'homePage',
+      },
+      tenant,
+      title: `detail 1 ${time}`,
+    });
+
+    // ensure correct language
+    const langSelect = await page.locator('.localizer.app-header__localizer');
+
+    await langSelect.click();
+
+    const germanLang = await page.locator('.localizer__locale-code[data-locale="de"]');
+
+    await germanLang.click({
+      force: true,
+    });
+
+    // go to detail page
+
+    await page.goto('http://localhost:3000/admin/collections/nationalDictionaryDetailPage?depth=1&limit=10');
     await page.waitForLoadState('networkidle');
 
     const detailPageEntry = await page.getByText(`detail 1 ${time}`);
