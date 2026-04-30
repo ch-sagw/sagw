90f8f573ff416554ec08e0f87995ae5b9d1f7386
Yves Tscherry <info@feerglas.com>
2026-04-30 11:01:22 +0200

    ci: try build first and then start

--- playwright-be.config.ts
+++ playwright-be.config.ts
@@ -52,7 +52,7 @@ export default defineConfig({
     trace: 'on-first-retry',
   },
   webServer: {
-    command: 'next dev --webpack -H 0.0.0.0 -p 3000',
+    command: 'next build --webpack && next start -H 0.0.0.0 -p 3000',
     env: {
 
       // issue: in some tests, we import payload config promise. on each,
