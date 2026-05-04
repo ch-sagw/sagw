5c1d04f75cbe7565ae30ffe614f56de11997ab3d
Marius Bleuer <m.bleuer@lycos.com>
2026-05-04 22:11:30 +0200

    feat: adding additional header logos for fg

--- src/components/base/HeaderLogo/HeaderLogo.stories.ts
+++ src/components/base/HeaderLogo/HeaderLogo.stories.ts
@@ -18,13 +18,96 @@ const meta: Meta<typeof HeaderLogo> = {
   parameters: {/* layout: 'centered', */ },
   tags: [
     'autodocs',
-    'visual:check',
     'a11y:check',
   ],
   title: 'Components/base/HeaderLogo',
 };
 
 export default meta;
+
+export const LogoCodicesDark: StrictStory = {
+  args: {
+    colorMode: 'dark',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'codices',
+  },
+  globals: {
+    backgrounds: {
+      value: 'dark',
+    },
+  },
+};
+
+export const LogoCodicesLight: StrictStory = {
+  args: {
+    colorMode: 'light',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'codices',
+  },
+  globals: {
+    backgrounds: {
+      value: 'light',
+    },
+  },
+};
+
+export const LogoCollromDark: StrictStory = {
+  args: {
+    colorMode: 'dark',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'collrom',
+  },
+  globals: {
+    backgrounds: {
+      value: 'dark',
+    },
+  },
+};
+
+export const LogoCollromLight: StrictStory = {
+  args: {
+    colorMode: 'light',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'collrom',
+  },
+  globals: {
+    backgrounds: {
+      value: 'light',
+    },
+  },
+};
+
+export const LogoSagDark: StrictStory = {
+  args: {
+    colorMode: 'dark',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'sag',
+  },
+  globals: {
+    backgrounds: {
+      value: 'dark',
+    },
+  },
+};
+
+export const LogoSagLight: StrictStory = {
+  args: {
+    colorMode: 'light',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'sag',
+  },
+  globals: {
+    backgrounds: {
+      value: 'light',
+    },
+  },
+};
 
 export const LogoSagwDark: StrictStory = {
   args: {
@@ -82,6 +165,454 @@ export const LogoSagoLight: StrictStory = {
   },
 };
 
+export const LogoSanasDark: StrictStory = {
+  args: {
+    colorMode: 'dark',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'sanas',
+  },
+  globals: {
+    backgrounds: {
+      value: 'dark',
+    },
+  },
+};
+
+export const LogoSanasLight: StrictStory = {
+  args: {
+    colorMode: 'light',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'sanas',
+  },
+  globals: {
+    backgrounds: {
+      value: 'light',
+    },
+  },
+};
+
+export const LogoSauteDark: StrictStory = {
+  args: {
+    colorMode: 'dark',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'saute',
+  },
+  globals: {
+    backgrounds: {
+      value: 'dark',
+    },
+  },
+};
+
+export const LogoSauteLight: StrictStory = {
+  args: {
+    colorMode: 'light',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'saute',
+  },
+  globals: {
+    backgrounds: {
+      value: 'light',
+    },
+  },
+};
+
+export const LogoSegDark: StrictStory = {
+  args: {
+    colorMode: 'dark',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'seg',
+  },
+  globals: {
+    backgrounds: {
+      value: 'dark',
+    },
+  },
+};
+
+export const LogoSegLight: StrictStory = {
+  args: {
+    colorMode: 'light',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'seg',
+  },
+  globals: {
+    backgrounds: {
+      value: 'light',
+    },
+  },
+};
+
+export const LogoSgaDark: StrictStory = {
+  args: {
+    colorMode: 'dark',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'sga',
+  },
+  globals: {
+    backgrounds: {
+      value: 'dark',
+    },
+  },
+};
+
+export const LogoSgaLight: StrictStory = {
+  args: {
+    colorMode: 'light',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'sga',
+  },
+  globals: {
+    backgrounds: {
+      value: 'light',
+    },
+  },
+};
+
+export const LogoSgasDark: StrictStory = {
+  args: {
+    colorMode: 'dark',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'sgas',
+  },
+  globals: {
+    backgrounds: {
+      value: 'dark',
+    },
+  },
+};
+
+export const LogoSgasLight: StrictStory = {
+  args: {
+    colorMode: 'light',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'sgas',
+  },
+  globals: {
+    backgrounds: {
+      value: 'light',
+    },
+  },
+};
+
+export const LogoSgavlDark: StrictStory = {
+  args: {
+    colorMode: 'dark',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'sgavl',
+  },
+  globals: {
+    backgrounds: {
+      value: 'dark',
+    },
+  },
+};
+
+export const LogoSgavlLight: StrictStory = {
+  args: {
+    colorMode: 'light',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'sgavl',
+  },
+  globals: {
+    backgrounds: {
+      value: 'light',
+    },
+  },
+};
+
+export const LogoSgbDark: StrictStory = {
+  args: {
+    colorMode: 'dark',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'sgb',
+  },
+  globals: {
+    backgrounds: {
+      value: 'dark',
+    },
+  },
+};
+
+export const LogoSgbLight: StrictStory = {
+  args: {
+    colorMode: 'light',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'sgb',
+  },
+  globals: {
+    backgrounds: {
+      value: 'light',
+    },
+  },
+};
+
+export const LogoSgbeDark: StrictStory = {
+  args: {
+    colorMode: 'dark',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'sgbe',
+  },
+  globals: {
+    backgrounds: {
+      value: 'dark',
+    },
+  },
+};
+
+export const LogoSgbeLight: StrictStory = {
+  args: {
+    colorMode: 'light',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'sgbe',
+  },
+  globals: {
+    backgrounds: {
+      value: 'light',
+    },
+  },
+};
+
+export const LogoSgbfDark: StrictStory = {
+  args: {
+    colorMode: 'dark',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'sgbf',
+  },
+  globals: {
+    backgrounds: {
+      value: 'dark',
+    },
+  },
+};
+
+export const LogoSgbfLight: StrictStory = {
+  args: {
+    colorMode: 'light',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'sgbf',
+  },
+  globals: {
+    backgrounds: {
+      value: 'light',
+    },
+  },
+};
+
+export const LogoSggDark: StrictStory = {
+  args: {
+    colorMode: 'dark',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'sgg',
+  },
+  globals: {
+    backgrounds: {
+      value: 'dark',
+    },
+  },
+};
+
+export const LogoSggLight: StrictStory = {
+  args: {
+    colorMode: 'light',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'sgg',
+  },
+  globals: {
+    backgrounds: {
+      value: 'light',
+    },
+  },
+};
+
+export const LogoSggfDark: StrictStory = {
+  args: {
+    colorMode: 'dark',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'sggf',
+  },
+  globals: {
+    backgrounds: {
+      value: 'dark',
+    },
+  },
+};
+
+export const LogoSggfLight: StrictStory = {
+  args: {
+    colorMode: 'light',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'sggf',
+  },
+  globals: {
+    backgrounds: {
+      value: 'light',
+    },
+  },
+};
+
+export const LogoSgjfDark: StrictStory = {
+  args: {
+    colorMode: 'dark',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'sgjf',
+  },
+  globals: {
+    backgrounds: {
+      value: 'dark',
+    },
+  },
+};
+
+export const LogoSgjfLight: StrictStory = {
+  args: {
+    colorMode: 'light',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'sgjf',
+  },
+  globals: {
+    backgrounds: {
+      value: 'light',
+    },
+  },
+};
+
+export const LogoSgksDark: StrictStory = {
+  args: {
+    colorMode: 'dark',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'sgks',
+  },
+  globals: {
+    backgrounds: {
+      value: 'dark',
+    },
+  },
+};
+
+export const LogoSgksLight: StrictStory = {
+  args: {
+    colorMode: 'light',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'sgks',
+  },
+  globals: {
+    backgrounds: {
+      value: 'light',
+    },
+  },
+};
+
+export const LogoSgmoikDark: StrictStory = {
+  args: {
+    colorMode: 'dark',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'sgmoik',
+  },
+  globals: {
+    backgrounds: {
+      value: 'dark',
+    },
+  },
+};
+
+export const LogoSgmoikLight: StrictStory = {
+  args: {
+    colorMode: 'light',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'sgmoik',
+  },
+  globals: {
+    backgrounds: {
+      value: 'light',
+    },
+  },
+};
+
+export const LogoSgrDark: StrictStory = {
+  args: {
+    colorMode: 'dark',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'sgr',
+  },
+  globals: {
+    backgrounds: {
+      value: 'dark',
+    },
+  },
+};
+
+export const LogoSgrLight: StrictStory = {
+  args: {
+    colorMode: 'light',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'sgr',
+  },
+  globals: {
+    backgrounds: {
+      value: 'light',
+    },
+  },
+};
+
+export const LogoSgssDark: StrictStory = {
+  args: {
+    colorMode: 'dark',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'sgss',
+  },
+  globals: {
+    backgrounds: {
+      value: 'dark',
+    },
+  },
+};
+
+export const LogoSgssLight: StrictStory = {
+  args: {
+    colorMode: 'light',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'sgss',
+  },
+  globals: {
+    backgrounds: {
+      value: 'light',
+    },
+  },
+};
+
 export const LogoSgtkDark: StrictStory = {
   args: {
     colorMode: 'dark',
@@ -103,6 +634,123 @@ export const LogoSgtkLight: StrictStory = {
     linkText: 'Back to Homepage',
     name: 'sgtk',
   },
+  globals: {
+    backgrounds: {
+      value: 'light',
+    },
+  },
+};
+
+export const LogoSmgDark: StrictStory = {
+  args: {
+    colorMode: 'dark',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'smg',
+  },
+  globals: {
+    backgrounds: {
+      value: 'dark',
+    },
+  },
+};
+
+export const LogoSmgLight: StrictStory = {
+  args: {
+    colorMode: 'light',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'smg',
+  },
+  globals: {
+    backgrounds: {
+      value: 'light',
+    },
+  },
+};
+
+export const LogoSpgDark: StrictStory = {
+  args: {
+    colorMode: 'dark',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'spg',
+  },
+  globals: {
+    backgrounds: {
+      value: 'dark',
+    },
+  },
+};
+
+export const LogoSpgLight: StrictStory = {
+  args: {
+    colorMode: 'light',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'spg',
+  },
+  globals: {
+    backgrounds: {
+      value: 'light',
+    },
+  },
+};
+
+export const LogoSsehDark: StrictStory = {
+  args: {
+    colorMode: 'dark',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'sseh',
+  },
+  globals: {
+    backgrounds: {
+      value: 'dark',
+    },
+  },
+};
+
+export const LogoSsehLight: StrictStory = {
+  args: {
+    colorMode: 'light',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'sseh',
+  },
+  globals: {
+    backgrounds: {
+      value: 'light',
+    },
+  },
+};
+
+export const LogoSsgDark: StrictStory = {
+  args: {
+    colorMode: 'dark',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'ssg',
+  },
+  globals: {
+    backgrounds: {
+      value: 'dark',
+    },
+  },
+};
+
+export const LogoSsgLight: StrictStory = {
+  args: {
+    colorMode: 'light',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'ssg',
+  },
+  globals: {
+    backgrounds: {
+      value: 'light',
+    },
+  },
 };
 
 export const LogoSslasDark: StrictStory = {
@@ -119,7 +767,7 @@ export const LogoSslasDark: StrictStory = {
   },
 };
 
-export const LogoSslaLight: StrictStory = {
+export const LogoSslasLight: StrictStory = {
   args: {
     colorMode: 'light',
     link: '/',
@@ -133,3 +781,58 @@ export const LogoSslaLight: StrictStory = {
   },
 };
 
+export const LogoSthgDark: StrictStory = {
+  args: {
+    colorMode: 'dark',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'sthg',
+  },
+  globals: {
+    backgrounds: {
+      value: 'dark',
+    },
+  },
+};
+
+export const LogoSthgLight: StrictStory = {
+  args: {
+    colorMode: 'light',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'sthg',
+  },
+  globals: {
+    backgrounds: {
+      value: 'light',
+    },
+  },
+};
+
+export const LogoSvavDark: StrictStory = {
+  args: {
+    colorMode: 'dark',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'svav',
+  },
+  globals: {
+    backgrounds: {
+      value: 'dark',
+    },
+  },
+};
+
+export const LogoSvavLight: StrictStory = {
+  args: {
+    colorMode: 'light',
+    link: '/',
+    linkText: 'Back to Homepage',
+    name: 'svav',
+  },
+  globals: {
+    backgrounds: {
+      value: 'light',
+    },
+  },
+};
--- src/components/base/HeaderLogo/HeaderLogo.tsx
+++ src/components/base/HeaderLogo/HeaderLogo.tsx
@@ -2,20 +2,71 @@ import { forwardRef } from 'react';
 import styles from '@/components/base/HeaderLogo/HeaderLogo.module.scss';
 import { ColorMode } from '@/components/base/types/colorMode';
 
+import codices from '@/components/base/HeaderLogo/logos/codices';
+import collrom from '@/components/base/HeaderLogo/logos/collrom';
 import notSagw from '@/components/base/HeaderLogo/logos/not-sagw';
+import sag from '@/components/base/HeaderLogo/logos/sag';
 import sago from '@/components/base/HeaderLogo/logos/sago';
 import sagw from '@/components/base/HeaderLogo/logos/sagw';
+import sanas from '@/components/base/HeaderLogo/logos/sanas';
+import saute from '@/components/base/HeaderLogo/logos/saute';
+import seg from '@/components/base/HeaderLogo/logos/seg';
+import sga from '@/components/base/HeaderLogo/logos/sga';
+import sgas from '@/components/base/HeaderLogo/logos/sgas';
+import sgavl from '@/components/base/HeaderLogo/logos/sgavl';
+import sgb from '@/components/base/HeaderLogo/logos/sgb';
+import sgbe from '@/components/base/HeaderLogo/logos/sgbe';
+import sgbf from '@/components/base/HeaderLogo/logos/sgbf';
+import sgg from '@/components/base/HeaderLogo/logos/sgg';
+import sggf from '@/components/base/HeaderLogo/logos/sggf';
+import sgjf from '@/components/base/HeaderLogo/logos/sgjf';
+import sgks from '@/components/base/HeaderLogo/logos/sgks';
+import sgmoik from '@/components/base/HeaderLogo/logos/sgmoik';
+import sgr from '@/components/base/HeaderLogo/logos/sgr';
+import sgss from '@/components/base/HeaderLogo/logos/sgss';
 import sgtk from '@/components/base/HeaderLogo/logos/sgtk';
+import smg from '@/components/base/HeaderLogo/logos/smg';
+import spg from '@/components/base/HeaderLogo/logos/spg';
+import sseh from '@/components/base/HeaderLogo/logos/sseh';
+import ssg from '@/components/base/HeaderLogo/logos/ssg';
 import sslas from '@/components/base/HeaderLogo/logos/sslas';
+import sthg from '@/components/base/HeaderLogo/logos/sthg';
+import svav from '@/components/base/HeaderLogo/logos/svav';
+
 import Link from 'next/link';
 
 export const Logos = {
+  codices,
+  collrom,
   // not-sagw will not be used in production
   'not-sagw': notSagw,
+  sag,
   sago,
   sagw,
+  sanas,
+  saute,
+  seg,
+  sga,
+  sgas,
+  sgavl,
+  sgb,
+  sgbe,
+  sgbf,
+  sgg,
+  sggf,
+  sgjf,
+  sgks,
+  sgmoik,
+  sgr,
+  sgss,
   sgtk,
+  smg,
+  spg,
+  sseh,
+  ssg,
   sslas,
+  sthg,
+  svav,
 };
 
 export interface InterfaceHeaderLogoPropTypes {
