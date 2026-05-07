66e5031155391f67452e1a936a5a35ecb69e8b85
Marius Bleuer <m.bleuer@lycos.com>
2026-05-07 13:52:26 +0200

    feat: adding additional header logos for fg (#1189)
    * feat: adding additional header logos for fg

--- src/components/base/HeaderLogo/logos/sgtk.tsx
+++ src/components/base/HeaderLogo/logos/sgtk.tsx
@@ -1,12 +1,15 @@
 import { JSX } from 'react';
+import styles from '@/components/base/HeaderLogo/HeaderLogo.module.scss';
 
 const icon = (): JSX.Element => (
   <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 277 48'>
-    <g>
-      <polygon points='19.28 1.46 19.28 29.54 26.81 29.54 26.81 1.46 19.28 1.46 19.28 1.46' fill='#e20613'/>
-      <polygon points='2 12.26 2 19.7 14.88 19.7 14.88 12.26 2 12.26 2 12.26' fill='#e20613'/>
-      <polygon points='30.89 12.18 30.89 19.62 43.77 19.62 43.77 12.18 30.89 12.18 30.89 12.18' fill='#e20613'/>
-      <polygon points='19.37 40.37 23.53 46.54 39.77 35.59 35.61 29.42 19.37 40.37 19.37 40.37' fill='#e20613'/>
+    <g className={styles['multicolor-logo']}>
+      <g className={styles['signet']}>
+        <polygon points='19.28 1.46 19.28 29.54 26.81 29.54 26.81 1.46 19.28 1.46 19.28 1.46' fill='#e20613'/>
+        <polygon points='2 12.26 2 19.7 14.88 19.7 14.88 12.26 2 12.26 2 12.26' fill='#e20613'/>
+        <polygon points='30.89 12.18 30.89 19.62 43.77 19.62 43.77 12.18 30.89 12.18 30.89 12.18' fill='#e20613'/>
+        <polygon points='19.37 40.37 23.53 46.54 39.77 35.59 35.61 29.42 19.37 40.37 19.37 40.37' fill='#e20613'/>
+      </g>
     </g>
   </svg>
 );
